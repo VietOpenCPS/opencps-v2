077b4ca3f70802e6fed2ea1bbebab51946a8b29d
Khoa Vu Dang <khoavd@fds.vn>
2017-09-27 10:59:07 +0700

    Add ServiceProcess term classes

---
+++ modules/backend-dossiermgt/backend-dossiermgt-api/src/main/java/org/opencps/dossiermgt/constants/ProcessActionTerm.java
@@ -0,0 +1,26 @@
+package org.opencps.dossiermgt.constants;
+
+public class ProcessActionTerm {
+	public static final String PROCESS_ACTION_ID = "processActionId";
+	public static final String GROUP_ID = "groupId";
+	public static final String USER_ID = "userId";
+	public static final String CREATE_DATE = "createDate";
+	public static final String MODIFIED_DATE = "modifiedDate";
+	public static final String SERVICE_PROCESS_ID = "serviceProcessId";
+	public static final String PRESTEP_CODE = "preStepCode";
+	public static final String POSTSTEP_CODE = "postStepCode";
+	public static final String AUTO_EVENT = "autoEvent";
+	public static final String PRE_CONDITION = "preCondition";
+	public static final String ACTION_CODE = "actionCode";
+	public static final String ACTION_NAME = "actionName";
+	public static final String ALLOW_ASSIGN_USER = "allowAssignUser";
+	public static final String ASSIGN_USER_ID = "assignUserId";
+	public static final String REQUEST_PAYMENT = "requestPayment";
+	public static final String PAYMENT_FEE = "paymentFee";
+	public static final String CREATE_DOSSIER_FILES = "createDossierFiles";
+	public static final String RETURN_DOSSIER_FILES = "returnDossierFiles";
+	public static final String MAKE_BRIEF_NOTE = "makeBriefNote";
+	public static final String SYNC_ACTION_CODE = "syncActionCode";
+	public static final String ROLLBACKABLE = "rollbackable";
+
+}
