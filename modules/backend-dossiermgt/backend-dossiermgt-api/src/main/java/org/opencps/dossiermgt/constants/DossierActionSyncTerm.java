e3689d08da4ab61537a7d977fbb9704eda64c814
Khoa Vu Dang <khoavd@fds.vn>
2017-10-04 15:13:55 +0700

    Add DossierTerm

---
+++ modules/backend-dossiermgt/backend-dossiermgt-api/src/main/java/org/opencps/dossiermgt/constants/DossierActionSyncTerm.java
@@ -0,0 +1,16 @@
+package org.opencps.dossiermgt.constants;
+
+public class DossierActionSyncTerm {
+	public static final String dossierActionSyncId = "dossierActionSyncId";
+	public static final String groupId = "groupId";
+	public static final String userId = "userId";
+	public static final String createDate = "createDate";
+	public static final String modifiedDate = "modifiedDate";
+	public static final String dossierId = "dossierId";
+	public static final String dossierActionId = "dossierActionId";
+	public static final String createDossier = "createDossier";
+	public static final String referenceUid = "referenceUid";
+	public static final String actionCode = "actionCode";
+	public static final String actionUser = "actionUser";
+	public static final String actionNote = "actionNote";
+}
---
+++ modules/backend-dossiermgt/backend-dossiermgt-api/src/main/java/org/opencps/dossiermgt/constants/DossierActionTerm.java
@@ -0,0 +1,26 @@
+package org.opencps.dossiermgt.constants;
+
+public class DossierActionTerm {
+	public static final String DOSSIERACTION_ID = "dossierActionId";
+	public static final String GROUP_ID = "groupId";
+	public static final String USER_ID = "userId";
+	public static final String CREATE_DATE = "createDate";
+	public static final String MODIFIED_DATE = "modifiedDate";
+	public static final String DOSSIER_ID = "dossierId";
+	public static final String SERVICE_PROCESS_ID = "serviceProcessId";
+	public static final String PREVIOUS_ACTION_ID = "previousActionId";
+	public static final String ACTION_CODE = "actionCode";
+	public static final String ACTION_USER = "actionUser";
+	public static final String ACTION_NAME = "actionName";
+	public static final String ACTION_NOTE = "actionNote";
+	public static final String ACTION_OVER_DUE = "actionOverdue";
+	public static final String SYNC_ACTION_CODE = "syncActionCode";
+	public static final String PENDING = "pending";
+	public static final String ROLLBACK_ABLE = "rollbackable";
+	public static final String STEP_CODE = "stepCode";
+	public static final String STEP_NAME = "stepName";
+	public static final String DUE_DATE = "dueDate";
+	public static final String NEXT_ACTION_ID = "nextActionId";
+	public static final String PAYLOAD = "payload";
+	public static final String STEP_INSTRUCTION = "stepInstruction";
+}
