ae4ff5c048f113efb7a28978d4a11494e60085d7
Khoa Vu Dang <khoavd@fds.vn>
2017-10-09 17:00:55 +0700

    Update dossiers

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/impl/DossierLocalServiceImpl.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/impl/DossierLocalServiceImpl.java
@@ -16,16 +16,27 @@ package org.opencps.dossiermgt.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import java.util.Date;
+
+import org.opencps.dossiermgt.model.Dossier;
 import org.opencps.dossiermgt.service.base.DossierLocalServiceBaseImpl;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 /**
  * The implementation of the dossier local service.
  *
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link org.opencps.dossiermgt.service.DossierLocalService} interface.
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link org.opencps.dossiermgt.service.DossierLocalService} interface.
  *
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
  *
  * @author huymq
@@ -37,6 +48,118 @@ public class DossierLocalServiceImpl extends DossierLocalServiceBaseImpl {
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
-	 * Never reference this class directly. Always use {@link org.opencps.dossiermgt.service.DossierLocalServiceUtil} to access the dossier local service.
+	 * Never reference this class directly. Always use {@link
+	 * org.opencps.dossiermgt.service.DossierLocalServiceUtil} to access the
+	 * dossier local service.
 	 */
+
+	public Dossier updateDossier(long groupId, long dossierId, String referenceUid, int counter, String serviceCode,
+			String serviceName, String govAgencyCode, String govAgencyName, String applicantName,
+			String applicantIdType, String applicantIdNo, Date applicantIdDate, String address, String cityCode,
+			String cityName, String districtCode, String districtName, String wardCode, String wardName,
+			String contactName, String contactTelNo, String contactEmail, String dossierTemplateNo, String dossierNote,
+			String submissionNote, String applicantNote, String briefNote, String dossierNo, boolean submitting,
+			Date correctingDate, String dossierStatus, String dossierStatusText, String dossierSubStatus,
+			String dossierSubStatusText, long folderId, long dossierActionId, int viaPostal, String postalAddress,
+			String postalCityCode, String postalCityName, String postalTelNo, String password, boolean notification,
+			boolean online, String serverNo, ServiceContext context) throws PortalException {
+
+		Date now = new Date();
+
+		long userId = context.getUserId();
+
+		User auditUser = userPersistence.fetchByPrimaryKey(userId);
+
+		validateUpdateDossier(groupId, dossierId, referenceUid, serviceCode, govAgencyCode, address, cityCode,
+				districtCode, wardCode, contactName, contactTelNo, contactEmail, dossierTemplateNo, dossierNote,
+				submissionNote, dossierNo, submitting, dossierStatusText, dossierSubStatusText, postalAddress,
+				postalCityCode, postalTelNo, serverNo);
+
+		Dossier dossier = null;
+
+		if (dossierId == 0) {
+			dossierId = counterLocalService.increment(Dossier.class.getName());
+
+			dossier = dossierPersistence.create(dossierId);
+
+			dossier.setCreateDate(now);
+			dossier.setModifiedDate(now);
+			dossier.setCompanyId(context.getCompanyId());
+			dossier.setGroupId(groupId);
+			dossier.setUserId(userId);
+			dossier.setUserName(auditUser.getFullName());
+
+			// Add extent fields
+			dossier.setReferenceUid(referenceUid);
+			dossier.setCounter(counter);
+			dossier.setServiceCode(serviceCode);
+			dossier.setServiceName(serviceName);
+			dossier.setGovAgencyCode(govAgencyCode);
+			dossier.setGovAgencyName(govAgencyName);
+			dossier.setApplicantName(applicantName);
+			dossier.setApplicantIdType(applicantIdType);
+			dossier.setApplicantIdNo(applicantIdNo);
+			dossier.setApplicantIdDate(applicantIdDate);
+			dossier.setDossierNo(dossierNo);
+			dossier.setApplicantNote(applicantNote);
+			dossier.setBriefNote(briefNote);
+
+			dossier.setDossierStatus(dossierStatus);
+			dossier.setDossierStatusText(dossierStatusText);
+			dossier.setDossierSubStatus(dossierSubStatus);
+			dossier.setDossierSubStatusText(dossierSubStatusText);
+
+			dossier.setFolderId(folderId);
+			dossier.setDossierActionId(dossierActionId);
+			dossier.setViaPostal(viaPostal);
+			dossier.setPostalAddress(postalAddress);
+			dossier.setPostalCityCode(postalCityCode);
+			dossier.setPostalCityName(postalCityName);
+			dossier.setPassword(password);
+			dossier.setNotification(notification);
+			dossier.setOnline(online);
+			dossier.setServerNo(serverNo);
+
+		} else {
+
+			dossier = dossierPersistence.fetchByPrimaryKey(dossierId);
+
+			dossier.setModifiedDate(now);
+
+			dossier.setApplicantName(applicantName);
+			dossier.setApplicantIdType(applicantIdType);
+			dossier.setApplicantIdNo(applicantIdNo);
+			dossier.setApplicantIdDate(applicantIdDate);
+			dossier.setAddress(postalAddress);
+			dossier.setCityCode(postalCityCode);
+			dossier.setCityName(postalCityName);
+			dossier.setDistrictCode(districtCode);
+			dossier.setDistrictName(districtName);
+			dossier.setWardCode(wardCode);
+			dossier.setWardName(wardName);
+			dossier.setContactName(contactName);
+			dossier.setContactEmail(contactEmail);
+			dossier.setViaPostal(viaPostal);
+			dossier.setPostalAddress(postalAddress);
+			dossier.setPostalCityCode(postalCityCode);
+			dossier.setPostalCityName(postalCityName);
+			dossier.setPostalTelNo(postalTelNo);
+			dossier.setApplicantNote(applicantNote);
+			dossier.setNotification(notification);
+
+		}
+
+		dossierPersistence.update(dossier);
+
+		return dossier;
+	}
+
+	private void validateUpdateDossier(long groupId, long dossierId, String referenceUid, String serviceCode,
+			String govAgencyCode, String address, String cityCode, String districtCode, String wardCode,
+			String contactName, String contactTelNo, String contactEmail, String dossierTemplateNo, String dossierNote,
+			String submissionNote, String dossierNo, boolean submitting, String dossierStatus, String dossierSubStatus,
+			String postalAddress, String postalCityCode, String postalTelNo, String serverNo) throws PortalException {
+
+	}
+
 }
