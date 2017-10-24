5398a7c86307156cfb319befca58fa64e1afdc11
Khoa Vu Dang <khoavd@fds.vn>
2017-10-24 17:17:29 +0700

    Update /dossiers APIs - POST - UPDATE - GET detail

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/DossierActions.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/DossierActions.java
@@ -18,6 +18,18 @@ public interface DossierActions {
 	public JSONObject getDossiers(long userId, long companyId, long groupId, LinkedHashMap<String, Object> params,
 			Sort[] sorts, int start, int end, ServiceContext serviceContext);
 
+	public Dossier initDossier(long groupId, long dossierId, String referenceUid, int counter, String serviceCode,
+			String serviceName, String govAgencyCode, String govAgencyName, String applicantName,
+			String applicantIdType, String applicantIdNo, String applicantIdDate, String address, String cityCode,
+			String cityName, String districtCode, String districtName, String wardCode, String wardName,
+			String contactName, String contactTelNo, String contactEmail, String dossierTemplateNo, String password,
+			int viaPostal, String postalAddress, String postalCityCode, String postalCityName, String postalTelNo,
+			boolean online, boolean notification, String applicantNote, ServiceContext context) throws PortalException;
+
+	public Dossier assignDossierToProcess(long dossierId, String dossierNote, String submissionNote, String briefNote,
+			String dossierNo, long folderId, long dossierActionId, String serverNo, ServiceContext context)
+			throws PortalException;
+
 	public Dossier addDossier(long groupId, long dossierId, String referenceUid, int counter, String serviceCode,
 			String serviceName, String govAgencyCode, String govAgencyName, String applicantName,
 			String applicantIdType, String applicantIdNo, Date applicantIdDate, String address, String cityCode,
@@ -33,25 +45,31 @@ public interface DossierActions {
 
 	public Dossier removeDossier(long groupId, long dossierId, String referenceUid) throws PortalException;
 
-	public Dossier cancelDossier(long groupId, long dossierId, String referenceUid, ServiceContext context) throws PortalException;
+	public Dossier cancelDossier(long groupId, long dossierId, String referenceUid, ServiceContext context)
+			throws PortalException;
 
-	public Dossier correctDossier(long groupId, long dossierId, String referenceUid, ServiceContext context) throws PortalException;
+	public Dossier correctDossier(long groupId, long dossierId, String referenceUid, ServiceContext context)
+			throws PortalException;
 
-	public Dossier submitDossier(long groupId, long dossierId, String referenceUid, ServiceContext context) throws PortalException;
+	public Dossier submitDossier(long groupId, long dossierId, String referenceUid, ServiceContext context)
+			throws PortalException;
 
-	public Dossier resetDossier(long groupId, long dossierId, String referenceUid, ServiceContext context) throws PortalException;
+	public Dossier resetDossier(long groupId, long dossierId, String referenceUid, ServiceContext context)
+			throws PortalException;
 
 	public List<ProcessAction> getNextActions(long groupId, long dossierId, String referenceUid) throws PortalException;
 
 	public List<DossierAction> getDossierActions(long groupId, long dossierId, String referenceUid)
 			throws PortalException;
 
-	public DossierAction doAction(long groupId, long dossierId, String referenceUid, String actionCode, long processActionId, String actionUser,
-			String actionNote, long assignUserId, long userId, ServiceContext context) throws PortalException;
+	public DossierAction doAction(long groupId, long dossierId, String referenceUid, String actionCode,
+			long processActionId, String actionUser, String actionNote, long assignUserId, long userId,
+			ServiceContext context) throws PortalException;
 
 	public Dossier markerVisited(long groupId, long dossierId, String referenceUid) throws PortalException;
 
-	public DossierAction doRollback(long groupId, long dossierId, String referenceUid, long userId) throws PortalException;
+	public DossierAction doRollback(long groupId, long dossierId, String referenceUid, long userId)
+			throws PortalException;
 
 	public JSONObject getContacts(long groupId, long dossierId, String referenceUid) throws PortalException;
 
