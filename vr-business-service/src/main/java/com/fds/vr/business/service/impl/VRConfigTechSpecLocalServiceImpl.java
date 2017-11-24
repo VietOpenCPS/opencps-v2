db86e4c906ecbad7264035cdb7ddf3382693fb35
admin <khoavd@fds.vn>
2017-11-24 09:37:06 +0700

    Init commit

---
+++ vr-business-service/src/main/java/com/fds/vr/business/service/impl/VRConfigTechSpecLocalServiceImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.fds.vr.business.service.impl;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.fds.vr.business.service.base.VRConfigTechSpecLocalServiceBaseImpl;
+
+/**
+ * The implementation of the vr config tech spec local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.fds.vr.business.service.VRConfigTechSpecLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author khoavd
+ * @see VRConfigTechSpecLocalServiceBaseImpl
+ * @see com.fds.vr.business.service.VRConfigTechSpecLocalServiceUtil
+ */
+@ProviderType
+public class VRConfigTechSpecLocalServiceImpl
+	extends VRConfigTechSpecLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link com.fds.vr.business.service.VRConfigTechSpecLocalServiceUtil} to access the vr config tech spec local service.
+	 */
+}
