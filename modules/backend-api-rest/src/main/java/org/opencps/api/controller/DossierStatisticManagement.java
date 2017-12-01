09884d889c8e5cccd8cb22c9649b0a56a770d49f
Nguyen Duc Au <mr.au1992@gmail.com>
2017-12-01 16:48:59 +0700

    no message

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/DossierStatisticManagement.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/DossierStatisticManagement.java
@@ -1,15 +1,29 @@
 package org.opencps.api.controller;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.opencps.api.dossierstatistic.model.DossierStatisticSearchModel;
+import org.opencps.api.serviceinfo.model.ServiceInfoSearchModel;
+
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 
 @Path("/dossierstatistic")
 @Api(value = "/dossierstatistic", tags = "dossierstatistic")
@@ -17,19 +31,22 @@ public interface DossierStatisticManagement {
 
 	@GET
 	@Path("/statistics/dossiers")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON ,MediaType.MULTIPART_FORM_DATA})
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.MULTIPART_FORM_DATA })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response getDossierStatistic();
-	
+	public Response getDossierStatistic(@Context HttpServletRequest request, @Context HttpHeaders header,
+			@Context Company company, @Context Locale locale, @Context User user,
+			@Context ServiceContext serviceContext,
+			@ApiParam(value = "query params for search") @BeanParam DossierStatisticSearchModel search);
+
 	@GET
 	@Path("/statistics/actions/year/{y}")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON ,MediaType.MULTIPART_FORM_DATA})
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.MULTIPART_FORM_DATA })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Response getYears(@PathParam("y") long year);
-	
+
 	@POST
 	@Path("/statistics/dossiers/agency/{code}")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON ,MediaType.MULTIPART_FORM_DATA})
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.MULTIPART_FORM_DATA })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Response postAgency(@PathParam("code") long agencyCd);
 }
--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierStatisticManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierStatisticManagementImpl.java
@@ -1,21 +1,40 @@
 package org.opencps.api.controller.impl;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 import org.opencps.api.controller.DossierStatisticManagement;
+import org.opencps.api.controller.exception.ErrorMsg;
+import org.opencps.api.controller.util.DossierStatisticUtils;
+import org.opencps.api.controller.util.ServiceInfoUtils;
+import org.opencps.api.dossierstatistic.model.DossierStatisticResultsModel;
+import org.opencps.api.dossierstatistic.model.DossierStatisticSearchModel;
+import org.opencps.api.serviceinfo.model.ServiceInfoResultsModel;
+import org.opencps.dossiermgt.action.ServiceInfoActions;
+import org.opencps.dossiermgt.action.impl.ServiceInfoActionsImpl;
+import org.opencps.dossiermgt.constants.DossierStatisticTerm;
+import org.opencps.dossiermgt.constants.ServiceInfoTerm;
 
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.SortFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 public class DossierStatisticManagementImpl implements DossierStatisticManagement {
 
 	Log _log = LogFactoryUtil.getLog(DossierStatisticManagementImpl.class);
-
-	@Override
-	public Response getDossierStatistic() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Response getYears(long year) {
@@ -27,6 +46,48 @@ public class DossierStatisticManagementImpl implements DossierStatisticManagemen
 	public Response postAgency(long agencyCd) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Response getDossierStatistic(HttpServletRequest request, HttpHeaders header, Company company, Locale locale,
+			User user, ServiceContext serviceContext, DossierStatisticSearchModel search) {
+		ServiceInfoActions actions = new ServiceInfoActionsImpl();
+
+		DossierStatisticResultsModel results = new DossierStatisticResultsModel();
+
+		try {
+
+			long groupId = GetterUtil.getLong(header.getHeaderString("groupId"));
+
+			LinkedHashMap<String, Object> params = new LinkedHashMap<String, Object>();
+
+			params.put(Field.GROUP_ID, String.valueOf(groupId));
+			params.put(DossierStatisticTerm.YEAR, search.getYear());
+			params.put(DossierStatisticTerm.MONTH, search.getMonth());
+			params.put(DossierStatisticTerm.DOMAIN_CODE, search.getDomainCode());
+			params.put(DossierStatisticTerm.GOV_AGENCY_CODE, search.getGovAgencyCode());
+			params.put(DossierStatisticTerm.ADMINISTRATION_LEVEL, search.getAdministrationLevel());
+
+			Sort[] sorts = new Sort[] {};
+
+			JSONObject jsonData = actions.getServiceInfos(serviceContext.getUserId(), serviceContext.getCompanyId(),
+					groupId, params, sorts, -1, 1, serviceContext);
+
+			results.setTotal(jsonData.getInt("total"));
+			results.getData().addAll(
+					DossierStatisticUtils.mappingToDossierStatistictModel((List<Document>) jsonData.get("data")));
+
+			return Response.status(200).entity(results).build();
+
+		} catch (Exception e) {
+			ErrorMsg error = new ErrorMsg();
+
+			error.setMessage("not found!");
+			error.setCode(404);
+			error.setDescription("not found!");
+
+			return Response.status(404).entity(error).build();
+		}
 	}
 
 }
