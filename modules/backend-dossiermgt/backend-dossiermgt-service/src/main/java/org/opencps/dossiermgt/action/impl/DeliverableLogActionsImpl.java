c21362c7e3b4f9341572a1f3a8e216be5ff7ae76
Nguyen Duc Au <mr.au1992@gmail.com>
2017-12-08 09:51:17 +0700

    no message

---
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DeliverableLogActionsImpl.java
@@ -0,0 +1,17 @@
+package org.opencps.dossiermgt.action.impl;
+
+import org.opencps.dossiermgt.action.DeliverableLogActions;
+import org.opencps.dossiermgt.model.DeliverableLog;
+import org.opencps.dossiermgt.service.DeliverableLogLocalServiceUtil;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+public class DeliverableLogActionsImpl implements DeliverableLogActions {
+
+	@Override
+	public DeliverableLog getDeliverableLog(Long id) throws PortalException {
+		DeliverableLog log = DeliverableLogLocalServiceUtil.getDeliverableLog(id);
+		return log;
+	}
+
+}
