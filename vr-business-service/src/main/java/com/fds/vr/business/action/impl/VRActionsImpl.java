608575a3228673b0f0aa3438812f2435912aa045
Vu Dang Khoa <khoavd.it@gmail.com>
2017-12-08 17:47:24 +0700

    module

--- vr-business-service/src/main/java/com/fds/vr/business/action/impl/VRActionsImpl.java
+++ vr-business-service/src/main/java/com/fds/vr/business/action/impl/VRActionsImpl.java
@@ -13,6 +13,7 @@ import org.opencps.datamgt.service.DictCollectionLocalServiceUtil;
 import org.opencps.datamgt.service.DictGroupLocalServiceUtil;
 import org.opencps.datamgt.service.DictItemGroupLocalServiceUtil;
 import org.opencps.datamgt.service.DictItemLocalServiceUtil;
+import org.opencps.dossiermgt.service.DossierFileLocalServiceUtil;
 
 import com.fds.vr.business.action.VRActions;
 import com.fds.vr.business.constant.VRKeys;
@@ -25,7 +26,6 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -39,70 +39,74 @@ public class VRActionsImpl implements VRActions {
 		JSONArray techSpecArr = JSONFactoryUtil.createJSONArray();
 
 		try {
-			// get id THONG_SO_KY_THUAT
-			DictCollection nhomThongSoKyThuat = DictCollectionLocalServiceUtil
-					.fetchByF_dictCollectionCode(VRKeys.NHOM_THONG_SO_KY_THUAT, groupId);
+			if (dossierFileId != 0) {
+				techSpecArr = getFormData(dossierFileId);
+			} else {
+				// get id THONG_SO_KY_THUAT
+				DictCollection nhomThongSoKyThuat = DictCollectionLocalServiceUtil
+						.fetchByF_dictCollectionCode(VRKeys.NHOM_THONG_SO_KY_THUAT, groupId);
 
-			long ttktId = nhomThongSoKyThuat.getPrimaryKey();
+				long ttktId = nhomThongSoKyThuat.getPrimaryKey();
 
-			DictGroup phanLoaiNhomTTSKT = DictGroupLocalServiceUtil.getByGC_GI_DCI(vehicleClass, groupId, ttktId);
+				DictGroup phanLoaiNhomTTSKT = DictGroupLocalServiceUtil.getByGC_GI_DCI(vehicleClass, groupId, ttktId);
 
-			List<DictItemGroup> danhSachNhomThongSoKTChiTiet = DictItemGroupLocalServiceUtil.findByDictGroupId(groupId,
-					phanLoaiNhomTTSKT.getPrimaryKey());
-			
-			
-			if (Validator.isNull(module)) {
-				module = "1";
-			}
+				List<DictItemGroup> danhSachNhomThongSoKTChiTiet = DictItemGroupLocalServiceUtil
+						.findByDictGroupId(groupId, phanLoaiNhomTTSKT.getPrimaryKey());
 
-			for (DictItemGroup dg : danhSachNhomThongSoKTChiTiet) {
-
-				DictItem dictItem = DictItemLocalServiceUtil.getDictItem(dg.getDictItemId());
-
-				List<VRConfigTechSpec> configTechSpecs = VRConfigTechSpecLocalServiceUtil.getByVCSC(vehicleClass,
-						dictItem.getItemCode(), module);
-
-				JSONObject jsonTechSpec = JSONFactoryUtil.createJSONObject();
-
-				jsonTechSpec.put("key", dictItem.getItemCode());
-				jsonTechSpec.put("type", "label");
-				jsonTechSpec.put("title", dictItem.getItemName());
-				jsonTechSpec.put("required", false);
-				jsonTechSpec.put("Reference", false);
-				jsonTechSpec.put("placeholder", dictItem.getItemDescription());
-				jsonTechSpec.put("datasource", StringPool.BLANK);
-				jsonTechSpec.put("value", StringPool.BLANK);
-
-				JSONArray items = JSONFactoryUtil.createJSONArray();
-
-				for (VRConfigTechSpec vrConfig : configTechSpecs) {
-					JSONObject techspec = JSONFactoryUtil.createJSONObject();
-
-					techspec.put("key", vrConfig.getSpecificationCode());
-
-					techspec.put("type", vrConfig.getSpecificationEntryType());
-
-					techspec.put("title", vrConfig.getSpecificationDisplayName());
-
-					techspec.put("required", vrConfig.getSpecificationMandatory());
-
-					techspec.put("Reference", false);
-
-					techspec.put("value", StringPool.BLANK);
-
-					techspec.put("standard", vrConfig.getSpecificationStandard());
-					techspec.put("basicunit", vrConfig.getSpecificationBasicUnit());
-					techspec.put("placeholder", vrConfig.getSpecificationEntryPlaceholder());
-					if (Validator.isNotNull(vrConfig.getSpecificationDataCollectionId())) {
-						techspec.put("datasource", getDataSource(vrConfig.getSpecificationDataCollectionId(), groupId));
-					}
-
-					items.put(techspec);
+				if (Validator.isNull(module)) {
+					module = "1";
 				}
 
-				jsonTechSpec.put("items", items);
+				for (DictItemGroup dg : danhSachNhomThongSoKTChiTiet) {
 
-				techSpecArr.put(jsonTechSpec);
+					DictItem dictItem = DictItemLocalServiceUtil.getDictItem(dg.getDictItemId());
+
+					List<VRConfigTechSpec> configTechSpecs = VRConfigTechSpecLocalServiceUtil.getByVCSC(vehicleClass,
+							dictItem.getItemCode(), module);
+
+					JSONObject jsonTechSpec = JSONFactoryUtil.createJSONObject();
+
+					jsonTechSpec.put("key", dictItem.getItemCode());
+					jsonTechSpec.put("type", "label");
+					jsonTechSpec.put("title", dictItem.getItemName());
+					jsonTechSpec.put("required", false);
+					jsonTechSpec.put("Reference", false);
+					jsonTechSpec.put("placeholder", dictItem.getItemDescription());
+					jsonTechSpec.put("datasource", StringPool.BLANK);
+					jsonTechSpec.put("value", StringPool.BLANK);
+
+					JSONArray items = JSONFactoryUtil.createJSONArray();
+
+					for (VRConfigTechSpec vrConfig : configTechSpecs) {
+						JSONObject techspec = JSONFactoryUtil.createJSONObject();
+
+						techspec.put("key", vrConfig.getSpecificationCode());
+
+						techspec.put("type", vrConfig.getSpecificationEntryType());
+
+						techspec.put("title", vrConfig.getSpecificationDisplayName());
+
+						techspec.put("required", vrConfig.getSpecificationMandatory());
+
+						techspec.put("Reference", false);
+
+						techspec.put("value", StringPool.BLANK);
+
+						techspec.put("standard", vrConfig.getSpecificationStandard());
+						techspec.put("basicunit", vrConfig.getSpecificationBasicUnit());
+						techspec.put("placeholder", vrConfig.getSpecificationEntryPlaceholder());
+						if (Validator.isNotNull(vrConfig.getSpecificationDataCollectionId())) {
+							techspec.put("datasource",
+									getDataSource(vrConfig.getSpecificationDataCollectionId(), groupId));
+						}
+
+						items.put(techspec);
+					}
+
+					jsonTechSpec.put("items", items);
+
+					techSpecArr.put(jsonTechSpec);
+				}
 
 			}
 
@@ -115,6 +119,81 @@ public class VRActionsImpl implements VRActions {
 		}
 
 		return returnObj;
+	}
+
+	private JSONArray getFormData(long dossierFileId) {
+
+		JSONArray output = JSONFactoryUtil.createJSONArray();
+
+		org.opencps.dossiermgt.model.DossierFile dossierFile = getDossierFile(dossierFileId);
+
+		JSONObject formData = null;
+
+		JSONArray formSchema = null;
+
+		if (Validator.isNotNull(dossierFile)) {
+
+			try {
+				formData = JSONFactoryUtil.createJSONObject(dossierFile.getFormData());
+
+				formSchema = JSONFactoryUtil.createJSONArray(dossierFile.getFormSchema());
+
+				int length = formSchema.length();
+
+				for (int i = 0; i < length; i++) {
+					JSONObject parent = formSchema.getJSONObject(i);
+
+					String keyObject = parent.getString("key");
+
+					String keyValue = formData.getString(keyObject);
+
+					parent.put("value", keyValue);
+
+					JSONArray items = JSONFactoryUtil.createJSONArray(parent.getString("items"));
+
+					int lengthChild = items.length();
+
+					JSONArray altJSONArray = JSONFactoryUtil.createJSONArray();
+
+					for (int j = 0; j < lengthChild; j++) {
+						JSONObject child = items.getJSONObject(j);
+
+						String childKeyObj = child.getString("key");
+
+						String childKeyValue = formData.getString(childKeyObj);
+
+						child.put("value", childKeyValue);
+
+						altJSONArray.put(child);
+					}
+
+					parent.put("items", altJSONArray);
+
+					output.put(parent);
+				}
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		return output;
+	}
+
+	private org.opencps.dossiermgt.model.DossierFile getDossierFile(long dossierFileId) {
+
+		org.opencps.dossiermgt.model.DossierFile dossierFile = null;
+
+		if (dossierFileId != 0) {
+			try {
+				dossierFile = DossierFileLocalServiceUtil.getDossierFile(dossierFileId);
+			} catch (Exception e) {
+				_log.error(e);
+			}
+
+		}
+
+		return dossierFile;
 	}
 
 	@Override
@@ -145,14 +224,12 @@ public class VRActionsImpl implements VRActions {
 			if (dictGroupId != 0) {
 				dictGroupName = DictGroupLocalServiceUtil.getDictGroup(dictGroupId).getGroupName();
 			}
-			
-			List<VRLimitConfigTechSpec> vrLimits =
-					VRLimitConfigTechSpecLocalServiceUtil.getLimitConfigs(
-						vehicleClass, dictGroupName, fomulaType);
-			
+
+			List<VRLimitConfigTechSpec> vrLimits = VRLimitConfigTechSpecLocalServiceUtil.getLimitConfigs(vehicleClass,
+					dictGroupName, fomulaType);
+
 			returnObj.put("status", HttpsURLConnection.HTTP_OK);
 			returnObj.put("content", getLimitsConfig(vrLimits));
-
 
 		} catch (Exception e) {
 			returnObj.put("status", HttpsURLConnection.HTTP_FORBIDDEN);
@@ -167,11 +244,11 @@ public class VRActionsImpl implements VRActions {
 			String vehicleClass, String vehicleType) {
 		JSONObject returnObj = JSONFactoryUtil.createJSONObject();
 		JSONArray techSpecArr = JSONFactoryUtil.createJSONArray();
-		
-		if(Validator.isNull(module)) {
+
+		if (Validator.isNull(module)) {
 			module = "1";
 		}
-		
+
 		try {
 			// get id THONG_SO_KY_THUAT
 			DictCollection nhomThongSoKyThuat = DictCollectionLocalServiceUtil
@@ -191,8 +268,8 @@ public class VRActionsImpl implements VRActions {
 				List<VRConfigTechSpec> configTechSpecs = VRConfigTechSpecLocalServiceUtil.getByVCCTSCMD(vehicleClass,
 						vehicleClass, dictItem.getItemCode(), module);
 
-				List<VRConfigTechSpec> configTechSpecsType = VRConfigTechSpecLocalServiceUtil.getByVCCTSCMD(vehicleClass,
-						vehicleType, dictItem.getItemCode(), module);
+				List<VRConfigTechSpec> configTechSpecsType = VRConfigTechSpecLocalServiceUtil
+						.getByVCCTSCMD(vehicleClass, vehicleType, dictItem.getItemCode(), module);
 
 				List<VRConfigTechSpec> configTechSpecsAll = new ArrayList<VRConfigTechSpec>();
 
@@ -260,13 +337,12 @@ public class VRActionsImpl implements VRActions {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	private JSONArray getLimitsConfig(List<VRLimitConfigTechSpec> vrLimitConfigs) {
 
 		JSONArray arrOut = JSONFactoryUtil.createJSONArray();
-		
-		for (VRLimitConfigTechSpec vrLimit : vrLimitConfigs) {
 
+		for (VRLimitConfigTechSpec vrLimit : vrLimitConfigs) {
 
 			arrOut.put(vrLimit.getSpecificationCode());
 		}
@@ -312,7 +388,7 @@ public class VRActionsImpl implements VRActions {
 
 			dictCollectionId = dc.getDictCollectionId();
 		} catch (Exception e) {
-			//_log.error(e);
+			// _log.error(e);
 		}
 
 		return dictCollectionId;
