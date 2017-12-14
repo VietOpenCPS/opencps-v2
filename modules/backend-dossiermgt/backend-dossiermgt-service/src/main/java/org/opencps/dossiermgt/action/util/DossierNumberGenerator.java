2c6174269dde2e831ef628c82c4825037fb0eac5
Nguyen Duc Au <mr.au1992@gmail.com>
2017-12-14 14:27:13 +0700

    no message

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/util/DossierNumberGenerator.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/util/DossierNumberGenerator.java
@@ -1,11 +1,15 @@
 package org.opencps.dossiermgt.action.util;
 
+import java.util.Calendar;
+import java.util.LinkedHashMap;
 import java.util.UUID;
 
-import org.opencps.auth.utils.UserUtils;
+import org.opencps.dossiermgt.constants.DossierTerm;
 import org.opencps.dossiermgt.service.DossierLocalServiceUtil;
-import org.opencps.usermgt.service.util.OCPSUserUtils;
 
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.util.PwdGenerator;
 import com.liferay.portal.kernel.util.StringPool;
 
@@ -17,12 +21,58 @@ public class DossierNumberGenerator {
 		return UUID.randomUUID().toString();
 	}
 
-	public static String generateDossierNumber(String pattern, long groupId) {
-		// TODO add more logic here for the generate by pattern
+	public static String generateDossierNumber(String code, String pattern, long groupId, Long companyId, String agency,
+			String service, String template) throws ParseException, SearchException {
+		LinkedHashMap<String, Object> param = new LinkedHashMap<String, Object>();
+		param.put(DossierTerm.AGENCY, agency);
+		param.put(DossierTerm.SERVICE, service);
+		param.put(DossierTerm.TEMPLATE, template);
+		SearchContext sc = new SearchContext();
+		sc.setCompanyId(companyId);
 
-		return UUID.randomUUID().toString();
+		String[] listPattern = pattern.split("/");
+		// Xử lý year
+		String year = listPattern[1].substring(1, listPattern[1].length() - 1);
+		Calendar calendar = Calendar.getInstance();
+		String y = "";
+		String processIdPattern = "";
+		if (year.length() == 2) {
+			y = String.valueOf(calendar.get(Calendar.YEAR)).substring(2);
+			// replace year
+			pattern = pattern.replace(listPattern[1], y);
+		} else {
+			y = String.valueOf(calendar.get(Calendar.YEAR));
+			// replace year
+			pattern = pattern.replace(listPattern[1], y);
+		}
+		//String processId = String.valueOf(DossierLocalServiceUtil.countLucene(param, sc) + 1)
+		String processId = "100";
+		String processP = "";
+		if (listPattern[0].equals("{code}")) {
+			// replace code
+			pattern = pattern.replace(listPattern[0], code);
+			processP = listPattern[2];
+		} else {
+			// replace code
+			pattern = pattern.replace(listPattern[2], code);
+			processP = listPattern[0];
+		}
+		processIdPattern = processP.substring(1, processP.length() - 1);
+		// replace procsesId
+		if (processId.length() == processIdPattern.length()) {
+			pattern = pattern.replace(processP, processId);
+		} else if (processId.length() > processIdPattern.length()) {
+			pattern = pattern.replace(processP,
+					processId.substring(processId.length() - processIdPattern.length() - 1));
+		} else {
+			String p = "%0" + (processIdPattern.length() - processId.length()) + "d%s";
+			pattern = pattern.replace(processP,
+					String.format(p,0, processId));
+		}
+
+		return pattern;
 	}
-
+	
 	public static int counterDossier(long userId, long groupId) {
 		// TODO add more logic for the case DOSSIER was created in Client by
 		// EMPLOYEE
