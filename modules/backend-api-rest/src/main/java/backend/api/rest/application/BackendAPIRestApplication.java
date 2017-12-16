5e0441517a17ce02d060c023a73dde9598d582c8
dtson <thaisonc8@gmail.com>
2017-12-16 13:41:01 +0700

    update registration

--- modules/backend-api-rest/src/main/java/backend/api/rest/application/BackendAPIRestApplication.java
+++ modules/backend-api-rest/src/main/java/backend/api/rest/application/BackendAPIRestApplication.java
@@ -1,115 +1,3 @@
-<<<<<<< HEAD
-package backend.api.rest.application;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-
-import org.opencps.api.context.provider.CompanyContextProvider;
-import org.opencps.api.context.provider.LocaleContextProvider;
-import org.opencps.api.context.provider.ServiceContextProvider;
-import org.opencps.api.context.provider.UserContextProvider;
-import org.opencps.api.controller.impl.ApplicantManagementImpl;
-import org.opencps.api.controller.impl.DataManagementImpl;
-import org.opencps.api.controller.impl.DossierActionManagementImpl;
-import org.opencps.api.controller.impl.DossierFileManagementImpl;
-import org.opencps.api.controller.impl.DossierLogManagementImpl;
-import org.opencps.api.controller.impl.DossierManagementImpl;
-import org.opencps.api.controller.impl.DossierSyncManagementImpl;
-import org.opencps.api.controller.impl.DossierTemplateManagementImpl;
-import org.opencps.api.controller.impl.EmployeeManagementImpl;
-import org.opencps.api.controller.impl.FileAttachManagementImpl;
-import org.opencps.api.controller.impl.HolidayManagementImpl;
-import org.opencps.api.controller.impl.JobposManagementImpl;
-import org.opencps.api.controller.impl.NotificationQueueManagementImpl;
-import org.opencps.api.controller.impl.NotificationTemplateImpl;
-import org.opencps.api.controller.impl.NotificationTypeManagementImpl;
-import org.opencps.api.controller.impl.OfficeSiteManagementImpl;
-import org.opencps.api.controller.impl.PaymentConfigManagementImpl;
-import org.opencps.api.controller.impl.PaymentFileManagementImpl;
-import org.opencps.api.controller.impl.ServerConfigManagementImpl;
-import org.opencps.api.controller.impl.ServiceConfigManagementImpl;
-import org.opencps.api.controller.impl.ServiceInfoManagementImpl;
-import org.opencps.api.controller.impl.ServiceProcessManagementImpl;
-import org.opencps.api.controller.impl.StatisticManagementImpl;
-import org.opencps.api.controller.impl.UserManagementImpl;
-import org.opencps.api.controller.impl.WorkTimeManagementImpl;
-import org.opencps.api.controller.impl.WorkingUnitManagementImpl;
-import org.opencps.dossiermgt.model.impl.DossierStatisticImpl;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-
-@ApplicationPath("/v2")
-@Component(immediate = true, service = Application.class)
-public class BackendAPIRestApplication extends Application {
-
-	public Set<Object> getSingletons() {
-		Set<Object> singletons = new HashSet<Object>();
-		
-		
-		// add REST endpoints (resources)
-		singletons.add(new ApplicantManagementImpl());
-		singletons.add(new ServiceInfoManagementImpl());
-		singletons.add( new ServiceConfigManagementImpl());
-		singletons.add( new DossierTemplateManagementImpl());
-		singletons.add(new ServiceProcessManagementImpl());
-		singletons.add(new PaymentConfigManagementImpl());
-		singletons.add(new PaymentFileManagementImpl());
-		singletons.add(new DossierManagementImpl());
-		singletons.add(new DossierFileManagementImpl());
-		singletons.add(new DossierActionManagementImpl());
-		singletons.add(new DossierLogManagementImpl());
-		singletons.add(new ServerConfigManagementImpl());
-		singletons.add(new DossierSyncManagementImpl());
-
-		singletons.add(new DataManagementImpl());
-		singletons.add(new HolidayManagementImpl());
-		singletons.add(new WorkTimeManagementImpl());
-		singletons.add(new NotificationTemplateImpl());
-		singletons.add(new NotificationTypeManagementImpl());
-		singletons.add(new NotificationQueueManagementImpl());
-		singletons.add(new OfficeSiteManagementImpl());
-		singletons.add(new WorkingUnitManagementImpl());
-		singletons.add(new JobposManagementImpl());
-		singletons.add(new UserManagementImpl());
-		singletons.add(new EmployeeManagementImpl());
-		singletons.add(new DossierStatisticImpl());
-		singletons.add(new FileAttachManagementImpl());
-		singletons.add(new StatisticManagementImpl());
-		// add service provider
-		singletons.add(_serviceContextProvider);
-		singletons.add(_companyContextProvider);
-		singletons.add(_localeContextProvider);
-		singletons.add(_userContextProvider);
-		
-		return singletons;	
-	}
-
-	@GET
-	@Path("chao")
-	@Produces("text/plain")
-	public String working() {
-		return "It works!";
-	}
-	
-	@Reference
-	private CompanyContextProvider _companyContextProvider;
-
-	@Reference
-	private LocaleContextProvider _localeContextProvider;
-
-	@Reference
-	private UserContextProvider _userContextProvider;
-
-	@Reference
-	private ServiceContextProvider _serviceContextProvider;
-
-=======
 package backend.api.rest.application;
 
 import java.util.HashSet;
@@ -144,6 +32,7 @@ import org.opencps.api.controller.impl.NotificationTypeManagementImpl;
 import org.opencps.api.controller.impl.OfficeSiteManagementImpl;
 import org.opencps.api.controller.impl.PaymentConfigManagementImpl;
 import org.opencps.api.controller.impl.PaymentFileManagementImpl;
+import org.opencps.api.controller.impl.RegistrationManagementImpl;
 import org.opencps.api.controller.impl.RegistrationTemplatesManagementImpl;
 import org.opencps.api.controller.impl.ServerConfigManagementImpl;
 import org.opencps.api.controller.impl.ServiceConfigManagementImpl;
@@ -195,6 +84,7 @@ public class BackendAPIRestApplication extends Application {
 		singletons.add(new StatisticManagementImpl());
 		singletons.add(new DeliverableTypesManagementImpl());
 		singletons.add(new RegistrationTemplatesManagementImpl());
+		singletons.add(new RegistrationManagementImpl());
 		
 		// add service provider
 		singletons.add(_serviceContextProvider);
@@ -224,5 +114,4 @@ public class BackendAPIRestApplication extends Application {
 	@Reference
 	private ServiceContextProvider _serviceContextProvider;
 
->>>>>>> c0d6ab5cf76868a2d6c5f71d8e158d255f5a556d
 }
--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/RegistrationActions.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/RegistrationActions.java
@@ -3,13 +3,19 @@ package org.opencps.dossiermgt.action;
 import org.opencps.dossiermgt.model.Registration;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 public interface RegistrationActions {
-	public void insert(Registration model);
-
-	public void update(Registration model);
+	public Registration update(Registration model) throws PortalException;
 
 	public void delete(long registrationId) throws PortalException;
 
 	public Registration getDetail(long registrationId) throws PortalException;
+
+	public Registration insert(long groupid, String applicantName, String applicantIdType, String applicantIdNo,
+			String applicantIdDate, String address, String cityCode, String cityName, String districtCode,
+			String districtName, String wardCode, String wardName, String contactName, String contactTelNo,
+			String contactEmail, String govAgencyCode, String govAgencyName, int registrationState,
+			String registrationClass, boolean submitting, ServiceContext serviceContext) throws SystemException, PortalException;
 }
