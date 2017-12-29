8c855419ba376092659e3bb7630da45ab1a6b155
binhthgc <binhth.alpaca@gmail.com>
2017-12-29 11:27:50 +0700

    autufill data + send check kyso

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/listenner/DossierFileListenner.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/listenner/DossierFileListenner.java
@@ -40,6 +40,19 @@ public class DossierFileListenner extends BaseModelListener<DossierFile> {
 		try {
 			DossierLogLocalServiceUtil.addDossierLog(model.getGroupId(), model.getDossierId(), model.getUserName(), content,
 					notificationType, payload, serviceContext);
+			
+			// Binhth add message bus to processing KySO file
+			Message message = new Message();
+			DossierPart dossierPart = DossierPartLocalServiceUtil.fetchByTemplatePartNo(model.getGroupId(),
+					model.getDossierTemplateNo(), model.getDossierPartNo());
+			
+			JSONObject msgDataESign = JSONFactoryUtil.createJSONObject();
+			msgDataESign.put("userId", model.getUserId());
+			msgDataESign.put("eSign", dossierPart.getESign());
+			msgDataESign.put("fileEntryId", model.getFileEntryId());
+
+			message.put("msgToEngine", msgDataESign);
+			MessageBusUtil.sendMessage("kyso/engine/out/destination", message);
 		} catch (SystemException | PortalException e) {
 			e.printStackTrace();
 		}
