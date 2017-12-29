fbbf7ee3a4bef188e5689635b996148dbb7106b6
huymq <huymq@fds.vn>
2017-12-29 13:57:30 +0700

    update registration sync form

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/scheduler/RegistrationSyncScheduler.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/scheduler/RegistrationSyncScheduler.java
@@ -116,7 +116,7 @@ public class RegistrationSyncScheduler extends BaseSchedulerEntryMessageListener
 							
 							Map<String, Object> paramsForm = getParamsPostRegistrationForm(registrationForm, registration.getUuid());
 							
-							JSONObject registrationFormPOSTrespone = rest.callPostAPI(desGroupId, HttpMethods.PUT, "application/json", 
+							JSONObject registrationFormPOSTrespone = rest.callPostAPI(desGroupId, HttpMethods.POST, "application/json", 
 									RESTFulConfiguration.SERVER_PATH_BASE, registrationFormEndpoint, RESTFulConfiguration.SERVER_USER,
 									RESTFulConfiguration.SERVER_PASS,
 									properties, paramsForm, serviceContext);
