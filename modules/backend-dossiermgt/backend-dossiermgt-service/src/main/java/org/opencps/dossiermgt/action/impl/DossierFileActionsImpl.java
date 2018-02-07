c9ec799edce705baf794fd9b504c3cc0d84bd5ea
Khoa VU <khoavd@fds.vn>
2018-02-07 16:16:49 +0700

    Get dossierFile by DossierPart

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierFileActionsImpl.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierFileActionsImpl.java
@@ -249,7 +249,9 @@ public class DossierFileActionsImpl implements DossierFileActions {
 	public void deleteAllDossierFile(long groupId, long dossierId, String fileTemplateNo,
 			ServiceContext serviceContext) throws PortalException {
 		
-		List<DossierFile> lsDossierFile = DossierFileLocalServiceUtil.getDossierFileByDID_FTNO(dossierId, fileTemplateNo, false);
+		//List<DossierFile> lsDossierFile = DossierFileLocalServiceUtil.getDossierFileByDID_FTNO(dossierId, fileTemplateNo, false);
+		
+		List<DossierFile> lsDossierFile =  DossierFileLocalServiceUtil.getDossierFileByDID_DPNO(dossierId, fileTemplateNo, false);
 		
 		_log.info("SIZE_DOSSIER_REMOVE_"+lsDossierFile.size());
 		
