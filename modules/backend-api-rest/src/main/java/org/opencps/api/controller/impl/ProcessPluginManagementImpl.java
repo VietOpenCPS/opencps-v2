f7428d52e9270d179c80b836bb6595b8cb009dcc
Khoa VU <khoavd@fds.vn>
2018-03-08 11:08:41 +0700

    Update preview for view PDF and HTMl

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/ProcessPluginManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/ProcessPluginManagementImpl.java
@@ -17,6 +17,8 @@ import org.opencps.auth.api.BackendAuth;
 import org.opencps.auth.api.BackendAuthImpl;
 import org.opencps.auth.api.exception.UnauthenticationException;
 import org.opencps.auth.api.exception.UnauthorizationException;
+import org.opencps.dossiermgt.action.DossierFileActions;
+import org.opencps.dossiermgt.action.impl.DossierFileActionsImpl;
 import org.opencps.dossiermgt.action.util.AutoFillFormData;
 import org.opencps.dossiermgt.model.Dossier;
 import org.opencps.dossiermgt.model.DossierAction;
@@ -45,6 +47,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 
 public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 
@@ -334,16 +337,20 @@ public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 
 					ProcessPlugin plugin = ProcessPluginLocalServiceUtil.getProcessPlugin(pluginid);
 
-					String formReport = plugin.getPluginForm();
+					// String formReport = plugin.getPluginForm();
 
-					String formData = plugin.getSampleData();
+					String formCode = plugin.getSampleData();
 
-					if (formData.startsWith("#")) {
-						formData = _getFormData(groupId, formData, dossier.getDossierId());
-					}
+					boolean autoRun = plugin.getAutoRun();
 
-					if (formReport.startsWith("#")) {
-						formReport = _getFormScript(formReport, dossier.getDossierId());
+					String formData = StringPool.BLANK;
+					String formReport = StringPool.BLANK;
+
+					if (formCode.startsWith("#")) {
+						formData = _getFormData(groupId, formCode, dossier.getDossierId(), autoRun,
+								dossier.getDossierTemplateNo());
+
+						formReport = _getFormScript(formCode, dossier.getDossierId());
 					}
 
 					Message message = new Message();
@@ -423,7 +430,8 @@ public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 
 	}
 
-	private String _getFormData(long groupId, String fileTemplateNo, long dossierId) {
+	private String _getFormData(long groupId, String fileTemplateNo, long dossierId, boolean autoRun,
+			String dossierTemplateNo) {
 
 		String formData = StringPool.BLANK;
 
@@ -441,6 +449,18 @@ public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 				DossierPart dossierPart = DossierPartLocalServiceUtil.getByFileTemplateNo(groupId, fileTemplateNo);
 
 				formData = AutoFillFormData.sampleDataBinding(dossierPart.getSampleData(), dossierId, serviceContext);
+
+				if (autoRun) {
+					// create DossierFile
+
+					DossierFileActions actions = new DossierFileActionsImpl();
+
+					actions.addDossierFile(groupId, dossierId, PortalUUIDUtil.generate(), dossierTemplateNo,
+							dossierPart.getPartNo(), fileTemplateNo, dossierPart.getPartName(), StringPool.BLANK, 0L,
+							null, StringPool.BLANK, String.valueOf(false), serviceContext);
+
+				}
+
 			} else {
 				formData = dossierFile.getFormData();
 			}
@@ -463,21 +483,31 @@ public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 
 			DossierPart part = DossierPartLocalServiceUtil.getByFileTemplateNo(dossier.getGroupId(), fileTemplateNo);
 
-			/*
-			 * DossierFile dossierFile =
-			 * DossierFileLocalServiceUtil.getDossierFileByDID_FTNO_First(
-			 * dossierId, fileTemplateNo, false, new
-			 * DossierFileComparator(false, "createDate", Date.class));
-			 * 
-			 */
-			// JSONObject jsFormScript =
-			// JSONFactoryUtil.createJSONObject(part.getFormScript());
 
 			formData = part.getFormReport();
 
-			// _log.info(formData);
 
-			// formData = dossierFile.getFormReport();
+		} catch (Exception e) {
+			_log.info("Cant get formdata with fileTemplateNo_" + fileTemplateNo);
+		}
+
+		return formData;
+	}
+	
+	private String _getFormHtml(String fileTemplateNo, long dossierId) {
+		String formData = StringPool.BLANK;
+
+		fileTemplateNo = StringUtil.replaceFirst(fileTemplateNo, "#", StringPool.BLANK);
+
+		try {
+
+			Dossier dossier = DossierLocalServiceUtil.getDossier(dossierId);
+
+			DossierPart part = DossierPartLocalServiceUtil.getByFileTemplateNo(dossier.getGroupId(), fileTemplateNo);
+
+
+			formData = part.getFormScript();
+
 
 		} catch (Exception e) {
 			_log.info("Cant get formdata with fileTemplateNo_" + fileTemplateNo);
@@ -486,6 +516,93 @@ public class ProcessPluginManagementImpl implements ProcessPluginManagement {
 		return formData;
 	}
 
+
 	Log _log = LogFactoryUtil.getLog(ProcessPluginManagementImpl.class);
+
+	@Override
+	public Response getPreviewHtml(HttpServletRequest request, HttpHeaders header, Company company, Locale locale,
+			User user, ServiceContext serviceContext, String id, long pluginid) {
+		BackendAuth auth = new BackendAuthImpl();
+
+		long groupId = GetterUtil.getLong(header.getHeaderString("groupId"));
+
+		try {
+
+			if (!auth.isAuth(serviceContext)) {
+				throw new UnauthenticationException();
+			}
+
+			Dossier dossier = getDossier(id, groupId);
+
+			if (Validator.isNotNull(dossier)) {
+
+				long dossierActionId = dossier.getDossierActionId();
+
+				if (dossierActionId != 0) {
+
+					ProcessPlugin plugin = ProcessPluginLocalServiceUtil.getProcessPlugin(pluginid);
+
+					// String formReport = plugin.getPluginForm();
+
+					String formCode = plugin.getSampleData();
+
+					boolean autoRun = plugin.getAutoRun();
+
+					String formData = StringPool.BLANK;
+					String formReport = StringPool.BLANK;
+
+					if (formCode.startsWith("#")) {
+						formData = _getFormData(groupId, formCode, dossier.getDossierId(), autoRun,
+								dossier.getDossierTemplateNo());
+
+						formReport = _getFormHtml(formCode, dossier.getDossierId());
+					}
+
+					JSONObject result = JSONFactoryUtil.createJSONObject();
+					
+					result.put("formReport", formReport);
+					result.put("formData", formData);
+					
+					return Response.status(HttpURLConnection.HTTP_OK).entity(JSONFactoryUtil.looseSerialize(result)).build();
+
+				} else {
+					throw new Exception("The dossier wasn't on process");
+				}
+
+			} else {
+				throw new Exception("Cant get dossier with id_" + id);
+			}
+
+		} catch (Exception e) {
+			_log.error(e);
+
+			ErrorMsg error = new ErrorMsg();
+
+			if (e instanceof UnauthenticationException) {
+				error.setMessage("Non-Authoritative Information.");
+				error.setCode(HttpURLConnection.HTTP_NOT_AUTHORITATIVE);
+				error.setDescription("Non-Authoritative Information.");
+
+				return Response.status(HttpURLConnection.HTTP_NOT_AUTHORITATIVE).entity(error).build();
+			} else {
+				if (e instanceof UnauthorizationException) {
+					error.setMessage("Unauthorized.");
+					error.setCode(HttpURLConnection.HTTP_NOT_AUTHORITATIVE);
+					error.setDescription("Unauthorized.");
+
+					return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).entity(error).build();
+
+				} else {
+
+					error.setMessage("Internal Server Error");
+					error.setCode(HttpURLConnection.HTTP_FORBIDDEN);
+					error.setDescription(e.getMessage());
+
+					return Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity(error).build();
+
+				}
+			}
+		}
+	}
 
 }
