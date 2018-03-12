3ed14969a0abfc6abd15ea99bcba97cca9b0b06a
Viet Lam <tranvietlam92@gmail.com>
2018-03-12 13:09:00 +0700

    update status payment file

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/impl/PaymentFileLocalServiceImpl.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/impl/PaymentFileLocalServiceImpl.java
@@ -46,6 +46,7 @@ import com.liferay.portal.kernel.search.ParseException;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
 import com.liferay.portal.kernel.search.generic.MultiMatchQuery;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -186,18 +187,25 @@ public class PaymentFileLocalServiceImpl extends PaymentFileLocalServiceBaseImpl
 
 		if (Validator.isNotNull(status)) {
 			String[] sliptStatus = status.split(StringPool.COMMA);
-			for (String strStatus : sliptStatus) {
-				if (Validator.isNotNull(strStatus)) {
-
-					MultiMatchQuery query = new MultiMatchQuery(strStatus);
-
-					query.addFields(PaymentFileTerm.PAYMENT_STATUS);
-					booleanQuery.add(query, BooleanClauseOccur.SHOULD);
+			if (sliptStatus != null && sliptStatus.length > 0) {
+			BooleanQuery subQuery = new BooleanQueryImpl();
+				for (String strStatus : sliptStatus) {
+					if (Validator.isNotNull(strStatus)) {
+	
+						MultiMatchQuery query = new MultiMatchQuery(strStatus);
+	
+						query.addFields(PaymentFileTerm.PAYMENT_STATUS);
+						subQuery.add(query, BooleanClauseOccur.SHOULD);
+					}
 				}
+				booleanQuery.add(subQuery, BooleanClauseOccur.MUST);
+			} else {
+				MultiMatchQuery query = new MultiMatchQuery(status);
+
+				query.addFields(PaymentFileTerm.PAYMENT_STATUS);
+
+				booleanQuery.add(query, BooleanClauseOccur.MUST);
 			}
-			// MultiMatchQuery query = new MultiMatchQuery(status);
-			// query.addFields(PaymentFileTerm.PAYMENT_STATUS);
-			// booleanQuery.add(query, BooleanClauseOccur.MUST);
 		}
 
 		if (Validator.isNotNull(isNew) && Boolean.parseBoolean(isNew)) {
