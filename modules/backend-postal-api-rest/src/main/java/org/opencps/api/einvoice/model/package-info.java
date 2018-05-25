5e360f9a934d2fe6401616a24cfc13e95d32791c
dtson <thaisonc8@gmail.com>
2018-05-25 10:28:28 +0700

    backend-evaluation & Invoice & vnpost

---
+++ modules/backend-postal-api-rest/src/main/java/org/opencps/api/einvoice/model/package-info.java
@@ -0,0 +1,9 @@
+@XmlSchema( 
+    namespace = "http://tempuri.org/", 
+    elementFormDefault = XmlNsForm.QUALIFIED,
+    xmlns = @XmlNs(prefix = "", namespaceURI = "http://tempuri.org/"))
+package org.opencps.api.einvoice.model;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlNsForm;
