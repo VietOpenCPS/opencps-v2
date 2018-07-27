6aaaf435f1103f97e90abeaca1d5e2908c3048f1
Vu Dang Khoa <khoavd@fds.vn>
2018-07-27 10:20:43 +0700

    Update API

--- modules/backend-statistic/opencps-statistic-rest/src/main/java/org/opencps/statistic/rest/application/OpencpsStatisticRestApplication.java
+++ modules/backend-statistic/opencps-statistic-rest/src/main/java/org/opencps/statistic/rest/application/OpencpsStatisticRestApplication.java
@@ -4,48 +4,69 @@ import java.util.Collections;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.opencps.statistic.rest.dto.DossierStatisticData;
+import org.opencps.statistic.rest.dto.DossierStatisticResponse;
 import org.osgi.service.component.annotations.Component;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import opencps.statistic.common.webservice.exception.OpencpsApiError;
-import opencps.statistic.common.webservice.exception.OpencpsApiErrorResponse;
+import opencps.statistic.common.webservice.exception.OpencpsServiceException;
+import opencps.statistic.common.webservice.exception.OpencpsServiceExceptionDetails;
+import opencps.statistic.common.webservice.exception.ServiceException;
 
 /**
  * @author khoavu
  */
 @ApplicationPath("/statistics")
 @Component(immediate = true, service = Application.class)
+@Consumes("application/json")
+@Produces("application/json")
 public class OpencpsStatisticRestApplication extends Application {
+
+	private final static Logger LOG = LoggerFactory.getLogger(OpencpsStatisticRestApplication.class);
 
 	public Set<Object> getSingletons() {
 		return Collections.<Object>singleton(this);
 	}
 
 	@GET
-	@Produces("text/plain")
-	public String working() {
+	public DossierStatisticResponse working() {
+
+		LOG.info("GET DossierStatisticResponse");
+
+		DossierStatisticResponse dossierStatisticResponse = new DossierStatisticResponse();
+		dossierStatisticResponse.setTotal(1);
+		DossierStatisticData dossierStatisticData = new DossierStatisticData();
+		dossierStatisticData.setBetimesCount(1);
+		dossierStatisticResponse.setDossierStatisticData(dossierStatisticData);
 		
-		return "It works!";
+		OpencpsServiceExceptionDetails serviceExceptionDetails = new OpencpsServiceExceptionDetails();
+		serviceExceptionDetails.setFaultCode("100");
+		serviceExceptionDetails.setFaultMessage("Student Name is not correct");
+		//throwException(new OpencpsServiceException(serviceExceptionDetails));
+
+		return dossierStatisticResponse;
 	}
-	
-	  public static void throwException(ServiceException serviceException) throws ServiceException {
-		    ResponseBuilder builder = Response.status(Response.Status.NOT_ACCEPTABLE);
-		    builder.type("application/json");
-		    builder.entity(serviceException.getFaultDetails());
-		    throw new WebApplicationException(builder.build());
-		  }
 
-	private ResponseEntity<Object> buildResponseEntity(OpencpsApiError oleApiError, HttpHeaders headers,
-			HttpStatus status) {
-
-		OpencpsApiErrorResponse oleApiErrorResponse = new OpencpsApiErrorResponse(oleApiError);
-		return new ResponseEntity<>(oleApiErrorResponse, headers, status);
+	/**
+	 * Handle Exception
+	 * 
+	 * @param serviceException
+	 * @throws ServiceException
+	 */
+	public static void throwException(OpencpsServiceException serviceException) throws ServiceException {
+		ResponseBuilder builder = Response.status(Response.Status.NOT_ACCEPTABLE);
+		builder.type("application/json");
+		builder.entity(serviceException.getFaultDetails());
+		throw new WebApplicationException(builder.build());
 	}
 
 }
--- modules/backend-statistic/opencps-statistic-rest/src/main/java/org/opencps/statistic/rest/dto/DossierStatisticResponse.java
+++ modules/backend-statistic/opencps-statistic-rest/src/main/java/org/opencps/statistic/rest/dto/DossierStatisticResponse.java
@@ -1,20 +1,29 @@
 package org.opencps.statistic.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "DossierStatisticResponse")
 public class DossierStatisticResponse {
 	private int total;
-	@JsonProperty("data")
+	@XmlElement(name = "data")
 	private DossierStatisticData dossierStatisticData;
+
 	public int getTotal() {
 		return total;
 	}
+
 	public void setTotal(int total) {
 		this.total = total;
 	}
+
 	public DossierStatisticData getDossierStatisticData() {
 		return dossierStatisticData;
 	}
+
 	public void setDossierStatisticData(DossierStatisticData dossierStatisticData) {
 		this.dossierStatisticData = dossierStatisticData;
 	}
