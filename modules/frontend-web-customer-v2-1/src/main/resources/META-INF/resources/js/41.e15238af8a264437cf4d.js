87bdaa32c87f4f6bd83a001616f01242efb0c912
hoang anh <hoanganh16091995@gmail.com>
2018-08-13 17:36:16 +0700

    update frontend supper gate & customer v2.1

---
+++ modules/frontend-web-customer-v2-1/src/main/resources/META-INF/resources/js/41.e15238af8a264437cf4d.js
@@ -0,0 +1,2 @@
+webpackJsonp([41],{238:function(e,n,o){var s=o(137)(null,null,null,null);s.options.__file="/home/hoanganh/Pictures/FrontEnd_Opencps2.1/frontend-opencps-v2.1/onegate_21_fe/src/components/ChamQuaHanDetail.vue",s.esModule&&Object.keys(s.esModule).some(function(e){return"default"!==e&&"__esModule"!==e})&&console.error("named exports are not supported in *.vue files."),e.exports=s.exports}});
+//# sourceMappingURL=41.e15238af8a264437cf4d.js.map
