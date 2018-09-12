9ff984f0996991e5c02b08215e6161d6aadfaa0d
Tran Viet Lam <lamtv@fds.vn>
2018-09-12 16:10:14 +0700

    update API postal

--- modules/backend-postal-api-rest/src/main/java/backend/postal/api/rest/controller/impl/EInvoiceManagementImpl.java
+++ modules/backend-postal-api-rest/src/main/java/backend/postal/api/rest/controller/impl/EInvoiceManagementImpl.java
@@ -2,10 +2,7 @@ package backend.postal.api.rest.controller.impl;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -29,7 +26,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.util.Validator;
 
 import backend.postal.api.rest.controller.EInvoiceManagement;
 
@@ -167,12 +163,12 @@ public class EInvoiceManagementImpl implements EInvoiceManagement {
 
 	}
 	
-	private String formatDate(String string, String formatType){
-		SimpleDateFormat sdf = new SimpleDateFormat(formatType);
-		if(Validator.isNull(string)){
-			Date now = new Date();
-			string = now.toString();
-		}
-		return sdf.format(string);
-	}
+//	private String formatDate(String string, String formatType){
+//		SimpleDateFormat sdf = new SimpleDateFormat(formatType);
+//		if(Validator.isNull(string)){
+//			Date now = new Date();
+//			string = now.toString();
+//		}
+//		return sdf.format(string);
+//	}
 }
