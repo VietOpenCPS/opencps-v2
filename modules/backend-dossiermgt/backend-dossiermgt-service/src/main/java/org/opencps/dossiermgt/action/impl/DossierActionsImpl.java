31f4cc55cd05095167d62a7a18af4457e8e24924
Tran Viet Lam <lamtv@fds.vn>
2018-09-26 17:50:04 +0700

    update log

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierActionsImpl.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierActionsImpl.java
@@ -2629,11 +2629,11 @@ public class DossierActionsImpl implements DossierActions {
 					}
 				}
 				catch (JSONException e) {
-					_log.error(e);
-//					e.printStackTrace();
+					_log.debug(e);
+					//_log.error(e);
 				} catch (ParseException e) {
-					_log.error(e);
-//					e.printStackTrace();
+					_log.debug(e);
+					//_log.error(e);
 				}
 //				_log.info("Fee amount: " + feeAmount + ", serviceAmount: " + serviceAmount + ", shipAmount: " + shipAmount);
 				PaymentFile oldPaymentFile = PaymentFileLocalServiceUtil.getByDossierId(groupId, dossier.getDossierId());
@@ -3591,6 +3591,7 @@ public class DossierActionsImpl implements DossierActions {
 		
 		//Calculate step due date
 		DossierAction dossierAction = DossierActionLocalServiceUtil.fetchDossierAction(dossier.getDossierActionId());
+		_log.info("dossierAction: "+dossierAction);
 		Date rootDate = now;
 		Date dueDate = null;
 		
@@ -3603,11 +3604,13 @@ public class DossierActionsImpl implements DossierActions {
 		}
 
 		Double durationCount = processStep.getDurationCount();
+		_log.info("durationCountStep: "+durationCount);
 		int durationUnit = serviceProcess.getDurationUnit();
 		
 //		_log.info("Calculate do action duration count: " + durationCount);
 		if (Validator.isNotNull(durationCount) && durationCount > 0) {
 			dueDate = HolidayUtils.getDueDate(rootDate, durationCount, durationUnit, dossier.getGroupId());
+			_log.info("dueDateAction: "+dueDate);
 		}		
 	
 		DossierLocalServiceUtil.updateDossier(dossier);
