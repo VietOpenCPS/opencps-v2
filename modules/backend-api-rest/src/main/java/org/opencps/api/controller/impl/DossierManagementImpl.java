0db892749faec8adf1ea9f6c8843875b3212390d
Tran Viet Lam <lamtv@fds.vn>
2018-10-18 11:30:50 +0700

    update send mail

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierManagementImpl.java
@@ -1566,6 +1566,9 @@ public class DossierManagementImpl implements DossierManagement {
 										queue.setToEmail(employee.getEmail());
 										queue.setToTelNo(employee.getTelNo());
 									}
+								} else {
+									queue.setToEmail(dossier.getContactEmail());
+									queue.setToTelNo(dossier.getContactTelNo());
 								}
 								
 								JSONObject payload = JSONFactoryUtil.createJSONObject();
