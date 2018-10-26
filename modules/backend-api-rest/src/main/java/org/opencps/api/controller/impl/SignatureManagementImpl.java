009dfe644e0e2363b5d95a19262a87774ee51e2b
dtson <thaisonc8@gmail.com>
2018-10-26 15:56:15 +0700

    cm

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/SignatureManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/SignatureManagementImpl.java
@@ -478,12 +478,9 @@ public class SignatureManagementImpl implements SignatureManagement{
 		//			_log.info("fileSigned Path: "+fileSigned.getAbsolutePath());
 		//			_log.info("fileSigned Name: "+fileSigned.getName());
 					DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.fetchDLFileEntry(fileEntryId);
-		//			_log.info("dlFileEntry: "+dlFileEntry.getFileName());
-		
 					DLAppLocalServiceUtil.updateFileEntry(user.getUserId(), dlFileEntry.getFileEntryId(), dlFileEntry.getTitle(),
 							dlFileEntry.getMimeType(), dlFileEntry.getTitle(), dlFileEntry.getDescription(),
 							StringPool.BLANK, true, fileSigned, serviceContext);
-		
 					// Update deliverable with deliverableType
 					DossierFile dossierFile = DossierFileLocalServiceUtil.getByFileEntryId(fileEntryId);
 					if (dossierFile != null) {
@@ -498,7 +495,7 @@ public class SignatureManagementImpl implements SignatureManagement{
 								}
 							}
 						}
-					}					
+					}
 				}
 			} else {
 //				result.put("msg", "fileEntryId");
