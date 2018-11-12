6cab1ffda4567d1c98605c655aa5f2bde8b2c5e6
Tran Viet Lam <lamtv@fds.vn>
2018-11-12 14:16:24 +0700

    update statistic

--- modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/application/OpencpsStatisticRestApplication.java
+++ modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/application/OpencpsStatisticRestApplication.java
@@ -333,20 +333,4 @@ public class OpencpsStatisticRestApplication extends Application {
 
 	}
 
-	private static String convertNormalDateToLuceneDate(String normal) {
-		if (Validator.isNull(normal)) {
-			return StringPool.BLANK;
-		}
-		String[] splitD = normal.split("/");
-		if (splitD.length != 3 ||
-				splitD[1].length() > 2 ||
-				splitD[0].length() > 2) {
-			return StringPool.BLANK;
-		}
-		String year = splitD[2];
-		String month = (splitD[1].length() == 1) ? "0" + splitD[1] : splitD[1];
-		String day = (splitD[0].length() == 1) ? "0" + splitD[0] : splitD[0];
-		
-		return year + month + day;
-	}
 }
