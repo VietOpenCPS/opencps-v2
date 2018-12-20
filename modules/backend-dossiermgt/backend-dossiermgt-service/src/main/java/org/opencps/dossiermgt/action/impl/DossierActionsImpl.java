924137cdb5c002b54c9cbc764300cb4ca0344e2c
Đặng Khánh Trung <trung.dk070384@gmail.com>
2018-12-20 14:38:30 +0700

    Update dossier publish

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierActionsImpl.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/impl/DossierActionsImpl.java
@@ -1799,7 +1799,8 @@ public class DossierActionsImpl implements DossierActions {
 															dossierPart.getPartNo());
 												} else {
 													formScript = dossierPart.getFormScript();
-													eForm = Validator.isNotNull(formScript) ? true : false;
+//													eForm = Validator.isNotNull(formScript) ? true : false;
+													eForm = dossierPart.getEForm();
 													formData = AutoFillFormData.sampleDataBinding(
 															dossierPart.getSampleData(), dossierId, serviceContext);
 //													_log.info("Dossier part: " + dossierPart.getPartNo());
@@ -2245,9 +2246,13 @@ public class DossierActionsImpl implements DossierActions {
 			DossierFile dossierFile = null;
 
 			try {
-				dossierFile = DossierFileLocalServiceUtil.getDossierFileByDID_FTNO_DPT_First(dossierId, fileTemplateNo,
-						2, false, new DossierFileComparator(false, "createDate", Date.class));
-
+				try {
+					dossierFile = DossierFileLocalServiceUtil.getDossierFileByDID_FTNO_DPT_First(dossierId, fileTemplateNo,
+							2, false, new DossierFileComparator(false, "createDate", Date.class));
+				}
+				catch (Exception e) {
+					
+				}
 				if (Validator.isNull(dossierFile)) {
 					DossierFileActions actions = new DossierFileActionsImpl();
 					dossierFile = actions.addDossierFile(groupId, dossierId, StringPool.BLANK, dossierTempNo,
