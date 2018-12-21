f9f973237754e33e03308dab1735622544a4efe3
Đặng Khánh Trung <trung.dk070384@gmail.com>
2018-12-21 16:03:50 +0700

    Update send mt

--- modules/backend-communication/backend-communication-service/src/main/java/org/opencps/communication/sms/utils/ViettelSMSUtils.java
+++ modules/backend-communication/backend-communication-service/src/main/java/org/opencps/communication/sms/utils/ViettelSMSUtils.java
@@ -1,7 +1,5 @@
 package org.opencps.communication.sms.utils;
 
-import com.viettel.smsbrand.CpBalance;
-
 import java.rmi.RemoteException;
 
 import javax.xml.rpc.ServiceException;
@@ -12,19 +10,13 @@ import ws.bulkSms.impl.Result;
 
 
 public class ViettelSMSUtils {
-	public static void main(String[] args) throws RemoteException, ServiceException {
-		System.out.println(sendSMS("", "", ""));
-	}
-	
-	public static String sendSMS(String body, String title, String toTelNo) throws RemoteException, ServiceException {
+	public static Result sendSMS(String body, String title, String toTelNo) throws RemoteException, ServiceException {
 		CcApi_ServiceLocator locator = new CcApi_ServiceLocator();
 		CcApi_PortType portType = locator.getCcApiPort();
 		
-//		Result result = portType.wsCpMt("viettelmcdt", "789456a@#123", 
-//				"VIETTELMCDT", "1", "84976969454", "84916676884", "ViettelMCDT", "bulksms", "Test SMS", "F");
+		Result result = portType.wsCpMt("viettelmcdt", "789456a@#123", 
+				"VIETTELMCDT", "1", "84976969454", toTelNo, "ViettelMCDT", "bulksms", body, "F");
 		
-		CpBalance result = portType.checkBalance("viettelmcdt", "789456a@#123", 
-				"VIETTELMCDT");
-		return result.getErrDesc();
+		return result;
 	}
 }
