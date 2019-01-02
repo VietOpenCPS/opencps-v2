65b53c8a230040458c25f9e38c1e2ceb4e609516
Tran Viet Lam <lamtv@fds.vn>
2019-01-02 10:18:27 +0700

    add time sheduler

--- modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/DossierStatisticEngine.java
+++ modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/DossierStatisticEngine.java
@@ -392,7 +392,7 @@ public class DossierStatisticEngine extends BaseSchedulerEntryMessageListener {
 	@Modified
 	protected void activate() {
 		schedulerEntryImpl.setTrigger(
-				TriggerFactoryUtil.createTrigger(getEventListenerClass(), getEventListenerClass(), 5, TimeUnit.MINUTE));
+				TriggerFactoryUtil.createTrigger(getEventListenerClass(), getEventListenerClass(), 10, TimeUnit.MINUTE));
 		_schedulerEngineHelper.register(this, schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
 	}
 
