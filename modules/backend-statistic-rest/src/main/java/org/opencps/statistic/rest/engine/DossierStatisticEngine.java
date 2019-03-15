c626fd9dbb8445ae9deea13efcd4a6594a96fe15
Đặng Khánh Trung <trung.dk070384@gmail.com>
2019-03-15 09:43:25 +0700

    Update statistic scheduler

--- modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/DossierStatisticEngine.java
+++ modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/DossierStatisticEngine.java
@@ -3,16 +3,20 @@ package org.opencps.statistic.rest.engine;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerException;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.StorageTypeAware;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
-import com.liferay.portal.kernel.scheduler.TriggerFactoryUtil;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -25,6 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.opencps.kernel.scheduler.StorageTypeAwareSchedulerEntryImpl;
 import org.opencps.statistic.exception.NoSuchOpencpsDossierStatisticException;
 import org.opencps.statistic.model.OpencpsDossierStatistic;
 import org.opencps.statistic.rest.dto.DossierStatisticData;
@@ -52,14 +57,12 @@ import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(immediate = true, service = DossierStatisticEngine.class)
-public class DossierStatisticEngine extends BaseSchedulerEntryMessageListener {
+public class DossierStatisticEngine extends BaseMessageListener {
 	private static volatile boolean isRunning = false;
 	
 	//private final static Logger LOG = LoggerFactory.getLogger(DossierStatisticEngine.class);
 	protected Log _log = LogFactoryUtil.getLog(DossierStatisticEngine.class);
 	
-	private SchedulerEngineHelper _schedulerEngineHelper;
-
 	public static final int GROUP_TYPE_SITE = 1;
 	
 	private OpencpsCallRestFacade<GetDossierRequest, GetDossierResponse> callDossierRestService = new OpencpsCallDossierRestFacadeImpl();
@@ -374,30 +377,88 @@ public class DossierStatisticEngine extends BaseSchedulerEntryMessageListener {
 		}
 	}
 
-	@Activate
-	@Modified
-	protected void activate() {
-		schedulerEntryImpl.setTrigger(
-				TriggerFactoryUtil.createTrigger(getEventListenerClass(), getEventListenerClass(), 10, TimeUnit.MINUTE));
-		_schedulerEngineHelper.register(this, schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
-	}
+	/**
+	   * activate: Called whenever the properties for the component change (ala Config Admin)
+	   * or OSGi is activating the component.
+	   * @param properties The properties map from Config Admin.
+	   * @throws SchedulerException in case of error.
+	   */
+	  @Activate
+	  @Modified
+	  protected void activate(Map<String,Object> properties) throws SchedulerException {
+		  String listenerClass = getClass().getName();
+		  Trigger jobTrigger = _triggerFactory.createTrigger(listenerClass, listenerClass, new Date(), null, 10, TimeUnit.MINUTE);
 
+		  _schedulerEntryImpl = new SchedulerEntryImpl(getClass().getName(), jobTrigger);
+		  _schedulerEntryImpl = new StorageTypeAwareSchedulerEntryImpl(_schedulerEntryImpl, StorageType.MEMORY_CLUSTERED);
+		  
+//		  _schedulerEntryImpl.setTrigger(jobTrigger);
+
+		  if (_initialized) {
+			  deactivate();
+		  }
+
+	    _schedulerEngineHelper.register(this, _schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+	    _initialized = true;
+	  }
+	  
 	@Deactivate
 	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+		if (_initialized) {
+			try {
+				_schedulerEngineHelper.unschedule(_schedulerEntryImpl, getStorageType());
+		    } catch (SchedulerException se) {
+		        if (_log.isWarnEnabled()) {
+		        	_log.warn("Unable to unschedule trigger", se);
+		        }
+		    }
+
+		      _schedulerEngineHelper.unregister(this);
+		}
+		_initialized = false;
 	}
 
+	/**
+	 * getStorageType: Utility method to get the storage type from the scheduler entry wrapper.
+	 * @return StorageType The storage type to use.
+	*/
+	protected StorageType getStorageType() {
+	    if (_schedulerEntryImpl instanceof StorageTypeAware) {
+	    	return ((StorageTypeAware) _schedulerEntryImpl).getStorageType();
+	    }
+	    
+	    return StorageType.MEMORY_CLUSTERED;
+	}
+	  
+	/**
+	   * setModuleServiceLifecycle: So this requires some explanation...
+	   * 
+	   * OSGi will start a component once all of it's dependencies are satisfied.  However, there
+	   * are times where you want to hold off until the portal is completely ready to go.
+	   * 
+	   * This reference declaration is waiting for the ModuleServiceLifecycle's PORTAL_INITIALIZED
+	   * component which will not be available until, surprise surprise, the portal has finished
+	   * initializing.
+	   * 
+	   * With this reference, this component activation waits until portal initialization has completed.
+	   * @param moduleServiceLifecycle
+	   */
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+	  
+	@Reference(unbind = "-")
+	protected void setTriggerFactory(TriggerFactory triggerFactory) {
+		_triggerFactory = triggerFactory;
 	}
 
 	@Reference(unbind = "-")
 	protected void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) {
-
 		_schedulerEngineHelper = schedulerEngineHelper;
 	}
-
-	@Reference(unbind = "-")
-	protected void setTriggerFactory(TriggerFactory triggerFactory) {
-	}
+	
+	private SchedulerEngineHelper _schedulerEngineHelper;
+	private TriggerFactory _triggerFactory;
+	private volatile boolean _initialized;
+	private SchedulerEntryImpl _schedulerEntryImpl = null;	
 }
--- modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/VotingStatisticScheduler.java
+++ modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/VotingStatisticScheduler.java
@@ -1,16 +1,22 @@
 package org.opencps.statistic.rest.engine;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerException;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.StorageTypeAware;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
-import com.liferay.portal.kernel.scheduler.TriggerFactoryUtil;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -22,6 +28,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.opencps.kernel.scheduler.StorageTypeAwareSchedulerEntryImpl;
 import org.opencps.statistic.rest.dto.GetVotingResultData;
 import org.opencps.statistic.rest.dto.GetVotingResultRequest;
 import org.opencps.statistic.rest.dto.GetVotingResultResponse;
@@ -44,12 +51,11 @@ import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(immediate = true, service = VotingStatisticScheduler.class)
-public class VotingStatisticScheduler extends BaseSchedulerEntryMessageListener {
+public class VotingStatisticScheduler extends BaseMessageListener {
 	private static volatile boolean isRunning = false;
 	//private final static Log _log = LogFactoryUtil.getLog(VotingStatisticScheduler.class);
-
-	private SchedulerEngineHelper _schedulerEngineHelper;
-
+	protected Log _log = LogFactoryUtil.getLog(VotingStatisticScheduler.class);
+	
 	public static final int GROUP_TYPE_SITE = 1;
 	OpencpsCallRestFacade<GetVotingResultRequest, GetVotingResultResponse> callVotingResultService = new OpencpsCallVotingRestFacadeImpl();
 
@@ -205,30 +211,88 @@ public class VotingStatisticScheduler extends BaseSchedulerEntryMessageListener 
 		}
 	}
 
-	@Activate
-	@Modified
-	protected void activate() {
-		schedulerEntryImpl.setTrigger(
-				TriggerFactoryUtil.createTrigger(getEventListenerClass(), getEventListenerClass(), 10, TimeUnit.MINUTE));
-		_schedulerEngineHelper.register(this, schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
-	}
+	/**
+	   * activate: Called whenever the properties for the component change (ala Config Admin)
+	   * or OSGi is activating the component.
+	   * @param properties The properties map from Config Admin.
+	   * @throws SchedulerException in case of error.
+	   */
+	  @Activate
+	  @Modified
+	  protected void activate(Map<String,Object> properties) throws SchedulerException {
+		  String listenerClass = getClass().getName();
+		  Trigger jobTrigger = _triggerFactory.createTrigger(listenerClass, listenerClass, new Date(), null, 10, TimeUnit.MINUTE);
 
+		  _schedulerEntryImpl = new SchedulerEntryImpl(getClass().getName(), jobTrigger);
+		  _schedulerEntryImpl = new StorageTypeAwareSchedulerEntryImpl(_schedulerEntryImpl, StorageType.MEMORY_CLUSTERED);
+		  
+//		  _schedulerEntryImpl.setTrigger(jobTrigger);
+
+		  if (_initialized) {
+			  deactivate();
+		  }
+
+	    _schedulerEngineHelper.register(this, _schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+	    _initialized = true;
+	  }
+	  
 	@Deactivate
 	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+		if (_initialized) {
+			try {
+				_schedulerEngineHelper.unschedule(_schedulerEntryImpl, getStorageType());
+		    } catch (SchedulerException se) {
+		        if (_log.isWarnEnabled()) {
+		        	_log.warn("Unable to unschedule trigger", se);
+		        }
+		    }
+
+		      _schedulerEngineHelper.unregister(this);
+		}
+		_initialized = false;
 	}
 
+	/**
+	 * getStorageType: Utility method to get the storage type from the scheduler entry wrapper.
+	 * @return StorageType The storage type to use.
+	*/
+	protected StorageType getStorageType() {
+	    if (_schedulerEntryImpl instanceof StorageTypeAware) {
+	    	return ((StorageTypeAware) _schedulerEntryImpl).getStorageType();
+	    }
+	    
+	    return StorageType.MEMORY_CLUSTERED;
+	}
+	  
+	/**
+	   * setModuleServiceLifecycle: So this requires some explanation...
+	   * 
+	   * OSGi will start a component once all of it's dependencies are satisfied.  However, there
+	   * are times where you want to hold off until the portal is completely ready to go.
+	   * 
+	   * This reference declaration is waiting for the ModuleServiceLifecycle's PORTAL_INITIALIZED
+	   * component which will not be available until, surprise surprise, the portal has finished
+	   * initializing.
+	   * 
+	   * With this reference, this component activation waits until portal initialization has completed.
+	   * @param moduleServiceLifecycle
+	   */
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+	  
+	@Reference(unbind = "-")
+	protected void setTriggerFactory(TriggerFactory triggerFactory) {
+		_triggerFactory = triggerFactory;
 	}
 
 	@Reference(unbind = "-")
 	protected void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) {
-
 		_schedulerEngineHelper = schedulerEngineHelper;
 	}
-
-	@Reference(unbind = "-")
-	protected void setTriggerFactory(TriggerFactory triggerFactory) {
-	}
+	
+	private SchedulerEngineHelper _schedulerEngineHelper;
+	private TriggerFactory _triggerFactory;
+	private volatile boolean _initialized;
+	private SchedulerEntryImpl _schedulerEntryImpl = null;	
 }
