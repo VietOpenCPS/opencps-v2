794561cac469c76797df726edbc6dd14fa9c6b98
Đặng Khánh Trung <trung.dk070384@gmail.com>
2019-07-04 23:31:19 +0700

    Update send mail notification

--- modules/backend-auth/src/main/java/org/opencps/kernel/message/email/MBEmailSenderImpl.java
+++ modules/backend-auth/src/main/java/org/opencps/kernel/message/email/MBEmailSenderImpl.java
@@ -6,6 +6,8 @@ import com.liferay.mail.kernel.service.MailService;
 import com.liferay.mail.kernel.service.MailServiceUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -19,14 +21,15 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(immediate = true, service = MBEmailSenderImpl.class)
 public class MBEmailSenderImpl implements MBEmailSender {
-
+	private Log _log = LogFactoryUtil.getLog(MBEmailSenderImpl.class);
+	
 	@Override
 	public void send(
 		MBMessageEntry messageEntry, String portletId,
 		ServiceContext... serviceContexts) {
 
 		if (messageEntry != null && messageEntry.isSendEmail() && messageEntry.getToAddress().length > 0) {
-			System.out.println("===SEND_MAIL_TO_ADD=======" + messageEntry.getToAddress()[0].getAddress());
+			_log.debug("===SEND_MAIL_TO_ADD=======" + messageEntry.getToAddress()[0].getAddress());
 			MailMessage mailMessage = new MailMessage();
 			mailMessage.setSubject(messageEntry.getEmailSubject());
 			mailMessage.setTo(messageEntry.getToAddress());
@@ -38,7 +41,7 @@ public class MBEmailSenderImpl implements MBEmailSender {
 			if (Validator.isNotNull(smtpUser)) {
 				messageEntry.getFrom().setAddress(smtpUser);				
 				mailMessage.setFrom(messageEntry.getFrom());
-				System.out.println("SEND EMAIL FROM: " + messageEntry.getFrom());
+				_log.debug("SEND EMAIL FROM: " + messageEntry.getFrom());
 			}
 			MailServiceUtil.sendEmail(mailMessage);
 
