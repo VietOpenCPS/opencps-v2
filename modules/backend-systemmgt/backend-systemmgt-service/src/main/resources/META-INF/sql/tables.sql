cd2eac078c91adde87e88dc43e1660dfadd5483b
Đặng Khánh Trung <trung.dk070384@gmail.com>
2019-12-26 09:08:14 +0700

    Update delete system

--- modules/backend-systemmgt/backend-systemmgt-service/src/main/resources/META-INF/sql/tables.sql
+++
@@ -1,9 +0,0 @@
-create table opencps_scheduler (
-	schedulerId LONG not null primary key,
-	schedulerType VARCHAR(255) null,
-	onTime DATE null,
-	nextTime DATE null,
-	expiredTime DATE null,
-	minDuration LONG,
-	maxDuration LONG
-);
