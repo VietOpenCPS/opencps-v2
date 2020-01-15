4aa61deb01f04d176a58ee6107ddfb2928d8d651
ThanhNV <nguyenthanh.ptiter@gmail.com>
2020-01-15 15:22:47 +0700

    BACKEND-JASPER-PROCESS: ATTT

---
+++ modules/backend-jasper-process/backend-jasper-process-service/src/main/java/backend/jasper/process/service/util/ConfigConstants.java
@@ -0,0 +1,22 @@
+package backend.jasper.process.service.util;
+
+public class ConfigConstants {
+
+	public static final String ENGINE_CLASSNAME_DOSSIERFILE = "opencps.open.engine.classname.dossierfile";
+	public static final String ENGINE_CLASSNAME_DELIVERABLE = "opencps.open.engine.classname.deliverable";
+	public static final String ENGINE_CLASSNAME_OPENCPSDELIVERABLE = "opencps.open.engine.classname.opencpsdeliverable";
+	public static final String ENGINE_CLASSNAME_DOSSIERDOCUMENT = "opencps.open.engine.classname.dossierdocument";
+	public static final String JASPER_DESTINATION_DOSSIER = "jasper/dossier/in/destination";
+	public static final String JASPER_DOCTYPE_PDF = "pdf";
+	public static final String JASPER_DOCTYPE_EXCEL = "excel";
+	public static final String JASPER_DOCTYPE_WORD = "word";
+
+	public static final String JASPER_TERM_USER_ID = "userId";
+	public static final String JASPER_TERM_CLASS_PK = "classPK";
+	public static final String JASPER_TERM_CLASS_NAME = "className";
+	public static final String JASPER_TERM_FORM_DATA = "formData";
+	public static final String JASPER_TERM_JRXML_TEMPLATE = "jrxmlTemplate";
+	public static final String JASPER_TERM_FILE_PATH = "filePath";
+	public static final String JASPER_TERM_MSG_TO_ENGINE = "msgToEngine";
+
+}
