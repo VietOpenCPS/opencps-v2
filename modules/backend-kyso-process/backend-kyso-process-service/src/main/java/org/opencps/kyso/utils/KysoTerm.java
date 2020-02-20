c1628eef505a3c2f5c1eff6bf556a8584169a0ce
Đặng Khánh Trung <trung.dk070384@gmail.com>
2020-02-20 13:32:06 +0700

    Update kyso module

--- modules/backend-kyso-process/backend-kyso-process-service/src/main/java/org/opencps/kyso/utils/KysoTerm.java
+++ modules/backend-kyso-process/backend-kyso-process-service/src/main/java/org/opencps/kyso/utils/KysoTerm.java
@@ -4,4 +4,22 @@ public class KysoTerm {
 	public static final String SERVER_TYPEKYSO = "type_kyso";
 	public static final String SERVER_TYPEDONGDAU = "type_dongdau";
 	public static final String SERVER_ACTIVE = "active";
+	public static final String OFFSET_X = "offsetX";
+	public static final String OFFSET_Y = "offsetY";
+	public static final String IMAGE_ZOOM = "imageZoom";
+	public static final String SHOW_SIGNATURE_INFO = "showSignatureInfo";
+	public static final String RESOURCES_FOLDER = "resources/";
+	public static final String CER_EXTENSION = ".cer";
+	public static final String PNG_EXTENSION = ".png";
+	public static final String CONDAU_PNG_EXTENSION = "_condau.png";
+	public static final String EXPORT_FOLDER = "/opt/";
+	public static final String HASH_COMPUTERS = "hashComputers";
+	public static final String SIGN_FIELD_NAMES = "signFieldNames";
+	public static final String FILE_PATHS = "filePaths";
+	public static final String FILE_NAMES = "fileNames";
+	public static final String MSG = "msg";
+	public static final String DOSSIER_FILE_IDS = "dossierFileIds";
+	public static final String DOSSIER_PART_IDS = "dossierPartIds";
+	public static final String INDEXS = "indexs";
+	public static final String INDEX_SIZES = "indexSizes";
 }
