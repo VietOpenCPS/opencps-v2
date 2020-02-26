4f97cb789885addd533a6dde93a70f2be4008520
unknown <trungnt@fds.vn>
2020-02-26 17:32:59 +0700

    update

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/ApplicantManagement.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/ApplicantManagement.java
@@ -264,5 +264,19 @@ public interface ApplicantManagement {
 			@ApiResponse(code = HttpURLConnection.HTTP_INTERNAL_ERROR, message = "Internal error", response = ExceptionModel.class) })
 	public Response validateSimpleCaptcha(@Context HttpServletRequest request, @Context HttpHeaders header, @Context Company company,
 			@Context Locale locale, @Context User user, @Context ServiceContext serviceContext,
-			@BeanParam ApplicantInputModel input, @FormParam("value") String value);	
+			@BeanParam ApplicantInputModel input, @FormParam("value") String value);
+	
+	
+	@POST
+	@Path("/updatemail")
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED })
+	@ApiOperation(value = "Register a applicant", response = ApplicantModel.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns a applicant was created", response = ApplicantModel.class),
+			@ApiResponse(code = HttpURLConnection.HTTP_FORBIDDEN, message = "Access denied", response = ExceptionModel.class),
+			@ApiResponse(code = HttpURLConnection.HTTP_INTERNAL_ERROR, message = "Internal error", response = ExceptionModel.class) })
+	public Response updateEmail(@Context HttpServletRequest request, @Context HttpHeaders header, @Context Company company,
+			@Context Locale locale, @Context User user, @Context ServiceContext serviceContext,
+			@FormParam("oldEmail") String oldEmail, @FormParam("newEmail") String newEmail);
 }
