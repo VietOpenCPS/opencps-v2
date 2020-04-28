df4f27b4815c944a63b4ba68c10fbf921dca4a4f
nguyenthanh <nguyenthanh.ptiter@gmail.com>
2020-04-28 10:30:35 +0700

    Applicant registry with indentifyNoFile

--- modules/backend-usermgt/backend-usermgt-api/src/main/java/org/opencps/usermgt/constants/ApplicantTerm.java
+++ modules/backend-usermgt/backend-usermgt-api/src/main/java/org/opencps/usermgt/constants/ApplicantTerm.java
@@ -65,9 +65,7 @@ public class ApplicantTerm {
 	public static final String EMAIL_UPDATE_SUCCESS = "email-update-success";
 	
 	public static final String INDENTIFY_DESTINATION = "applicant-indentify";
-	public static final String INDENTIFY_NO_F = "indentifyNoFFile";
-	public static final String INDENTIFY_NO_F_URL = "indentifyNoFFileUrl";
-	public static final String INDENTIFY_NO_B = "indentifyNoBFile";
-	public static final String INDENTIFY_NO_B_URL = "indentifyNoBFileUrl";
+	public static final String INDENTIFY_NO = "indentifyNoFFile";
+	public static final String INDENTIFY_NO_URL = "indentifyNoFFileUrl";
 	public static final String DOC_URL = "documents/"; 
 }
