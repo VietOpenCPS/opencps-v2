3458f14afaf588b577e82b97b9741d02f0690e31
yuvytung <yuvytung@gmail.com>
2020-05-13 15:22:29 +0700

    access Statistics _ top url user access

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/AccessStatisticsManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/AccessStatisticsManagementImpl.java
@@ -1,13 +1,21 @@
 package org.opencps.api.controller.impl;
 
+import backend.auth.api.exception.BusinessExceptionImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import org.apache.commons.httpclient.util.HttpURLConnection;
 import org.opencps.api.accessstatistics.model.AccessStatistics;
+import org.opencps.api.constants.ConstantUtils;
 import org.opencps.api.controller.AccessStatisticsManagement;
+import org.opencps.api.controller.util.MessageUtil;
+import org.opencps.auth.api.BackendAuth;
+import org.opencps.auth.api.BackendAuthImpl;
+import org.opencps.auth.api.exception.UnauthenticationException;
 import org.opencps.usermgt.action.AccessStatisticsActions;
 import org.opencps.usermgt.action.impl.AccessStatisticsActionsImpl;
 
@@ -19,6 +27,7 @@ import java.util.Map;
 
 public class AccessStatisticsManagementImpl implements AccessStatisticsManagement
 {
+	private static Log _log = LogFactoryUtil.getLog(AccessStatisticsManagementImpl.class);
 
 	@Override
 	public Response getAccessStatistics(HttpServletRequest request,HttpHeaders header,Company company,Locale locale,
@@ -141,5 +150,40 @@ public class AccessStatisticsManagementImpl implements AccessStatisticsManagemen
 		AccessStatisticsActions actions = new AccessStatisticsActionsImpl();
 		JSONObject jsonObject = actions.getOnline();
 		return Response.status(HttpURLConnection.HTTP_OK).entity(JSONFactoryUtil.serialize(jsonObject.toString())).build();
+	}
+
+	@Override
+	public Response getUserAccessStatistics(HttpServletRequest request,HttpHeaders header,Company company,Locale locale,
+		User user,ServiceContext serviceContext)
+	{
+		BackendAuth auth = new BackendAuthImpl();
+		backend.auth.api.BackendAuth auth2 = new backend.auth.api.BackendAuthImpl();
+
+		try
+		{
+
+			if (!auth.isAuth(serviceContext))
+			{
+				throw new UnauthenticationException();
+			}
+			if (!auth2.isAdmin(serviceContext,ConstantUtils.ROLE_ADMIN_LOWER))
+			{
+				return Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).entity(MessageUtil.getMessage(ConstantUtils.API_USER_NOTHAVEPERMISSION)).build();
+			}
+
+			long userId = serviceContext.getUserId();
+			String sessionId = request.getSession().getId();
+			AccessStatisticsActions actions = new AccessStatisticsActionsImpl();
+			JSONObject jsonObject = actions.getUserAccessStatistics(userId , sessionId);
+
+			return Response.status(HttpURLConnection.HTTP_OK).entity(JSONFactoryUtil.serialize(jsonObject.toString())).build();
+		}
+		catch (Exception e)
+		{
+			_log.error(e);
+			return BusinessExceptionImpl.processException(e);
+		}
+
+
 	}
 }
