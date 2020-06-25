62b7f88c0ae286ea08d5b61a3ee7905cb560cd8a
yuvytung <yuvytung@gmail.com>
2020-06-25 09:33:25 +0700

    fix originality

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/util/NotificationUtil.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/action/util/NotificationUtil.java
@@ -3,27 +3,30 @@ package org.opencps.dossiermgt.action.util;
 public class NotificationUtil {
 	private static final String SMS_TRUE = "sms=true";
 	private static final String EMAIL_TRUE = "email=true";
+
+	private static final String SMS_FALSE = "sms=false";
+	private static final String EMAIL_FALSE = "email=false";
 	private static final String COMMA = ",";
 	
 	public static boolean isSendSMS(String condition) {
 		String[] conditionArr = condition.split(COMMA);
 		for (String tempCondition : conditionArr) {
-			if (SMS_TRUE.equalsIgnoreCase(tempCondition)) {
+			if (SMS_TRUE.equalsIgnoreCase(tempCondition))
 				return true;
-			}
+			else if (SMS_FALSE.equalsIgnoreCase(tempCondition))
+				return false;
 		}
-		
-		return false;
+		return true;
 	}
 
 	public static boolean isSendEmail(String condition) {
 		String[] conditionArr = condition.split(COMMA);
 		for (String tempCondition : conditionArr) {
-			if (EMAIL_TRUE.equalsIgnoreCase(tempCondition)) {
+			if (EMAIL_TRUE.equalsIgnoreCase(tempCondition))
 				return true;
-			}
+			else if (EMAIL_FALSE.equalsIgnoreCase(tempCondition))
+				return false;
 		}
-		
-		return false;
+		return true;
 	}
 }
