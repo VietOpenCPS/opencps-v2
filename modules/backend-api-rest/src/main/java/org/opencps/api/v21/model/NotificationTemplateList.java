080eafdd7a6eb7bfb5892db69ee92027c7eba504
Giaonn <nguyenngocgyao93@gmail.com>
2020-07-06 10:12:07 +0700

    Add feature customer resend email

--- modules/backend-api-rest/src/main/java/org/opencps/api/v21/model/NotificationTemplateList.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/v21/model/NotificationTemplateList.java
@@ -10,6 +10,7 @@ package org.opencps.api.v21.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.FormParam;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -211,6 +212,7 @@ public class NotificationTemplateList {
     public static class NotificationTemplate {
 
         @XmlElement(required = true)
+        @FormParam(value = "notificationType")
         protected String notificationType;
         protected boolean sendEmail;
         @XmlElement(required = true)
