38818931760698c86ecbc22c52f48967f467f6bd
NguyenDuong <nh0xkid.chinhlata0@gmail.com>
2020-08-18 14:37:38 +0700

    BQP: Fix domainCode

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/indexer/DossierIndexer.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/service/indexer/DossierIndexer.java
@@ -790,7 +790,7 @@ public class DossierIndexer extends BaseIndexer<Dossier> {
 					domainName = service.getDomainName();
 				}
 			}
-			document.addTextSortable(DossierTerm.DOMAIN_CODE, domainCode);
+			document.addTextSortable(DossierTerm.DOMAIN_CODE,domainCode);
 			document.addTextSortable(DossierTerm.DOMAIN_CODE_SEARCH, SpecialCharacterUtils.splitSpecial(domainCode));
 			document.addTextSortable(DossierTerm.DOMAIN_NAME, domainName);
 			document.addTextSortable(DossierTerm.ORIGIN_DOSSIER_ID, String.valueOf(object.getOriginDossierId()));
