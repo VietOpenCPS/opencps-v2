c9db2713ee0916834c8bbfaa9f0f3e1af5eecd25
nhan0987 <skynetx002@gmail.com>
2020-12-24 17:20:21 +0700

    Thêm auto upgrade db cho module sync tracking

--- modules/sync-tracking/sync-tracking-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/sync-tracking/sync-tracking-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,4 +1,3 @@
-create index IX_347F1D6B on opencps_synctracking (groupId, createDate, createDate);
 create index IX_5D8AF1B8 on opencps_synctracking (groupId, createDate, modifiedDate);
 create index IX_FC376796 on opencps_synctracking (groupId, dossierNo[$COLUMN_LENGTH:75$], createDate, modifiedDate);
 create index IX_C61B1D01 on opencps_synctracking (groupId, protocol[$COLUMN_LENGTH:75$], dossierNo[$COLUMN_LENGTH:75$]);
