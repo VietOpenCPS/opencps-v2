6b9f5aba5105e3275244dce526b1766a0a414a81
duongnguyen <nh0xkid.chinhlata0@gmail.com>
2021-02-04 16:18:06 +0700

    Cập nhật hồ sơ quá hạn về đúng hạn

--- modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierManagementImpl.java
+++ modules/backend-api-rest/src/main/java/org/opencps/api/controller/impl/DossierManagementImpl.java
@@ -9060,11 +9060,16 @@ public class DossierManagementImpl implements DossierManagement {
 			List<Role> userRoles = user.getRoles();
 			boolean overdue = false;
 			for (Role r : userRoles) {
-				r.setName(ConstantUtils.ROLE_OVERDUE);
-				if (r.getName().startsWith(ConstantUtils.ROLE_OVERDUE)) {
-					_log.info("Role TRUE");
+				if(r.getName().startsWith(ConstantUtils.GLOBAL_EDIT_OVERDUE)) {
 					overdue = true;
 					break;
+				}else {
+					r.setName(ConstantUtils.ROLE_OVERDUE);
+					if (r.getName().startsWith(ConstantUtils.ROLE_OVERDUE)) {
+						_log.info("Role TRUE");
+						overdue = true;
+						break;
+					}
 				}
 			}
 
