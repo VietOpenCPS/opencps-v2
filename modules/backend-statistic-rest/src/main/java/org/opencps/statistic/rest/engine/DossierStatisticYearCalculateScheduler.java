452b24a48c086ff8801503e8bc574caec0bc5809
nhan0987 <skynetx002@gmail.com>
2021-02-17 11:08:47 +0700

    Thêm scheduler tổng hợp số liệu hồ sơ theo năm

---
+++ modules/backend-statistic-rest/src/main/java/org/opencps/statistic/rest/engine/DossierStatisticYearCalculateScheduler.java
@@ -0,0 +1,170 @@
+package org.opencps.statistic.rest.engine;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.*;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.persistence.CompanyUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.Validator;
+import org.opencps.communication.model.ServerConfig;
+import org.opencps.communication.service.ServerConfigLocalServiceUtil;
+import org.opencps.kernel.scheduler.StorageTypeAwareSchedulerEntryImpl;
+import org.opencps.statistic.model.OpencpsDossierStatistic;
+import org.opencps.statistic.rest.dto.DossierStatisticData;
+import org.opencps.statistic.rest.dto.DossierStatisticKey;
+import org.opencps.statistic.rest.engine.service.StatisticEngineUpdate;
+import org.opencps.statistic.rest.engine.service.StatisticEngineUpdateAction;
+import org.opencps.statistic.rest.util.DossierStatisticConstants;
+import org.opencps.statistic.service.OpencpsDossierStatisticLocalServiceUtil;
+import org.osgi.service.component.annotations.*;
+
+import java.util.*;
+
+@Component(immediate = true, property = { "cron.expression=0 0 1 1 1/1 ? *" }, service = DossierStatisticYearCalculateScheduler.class)
+public class DossierStatisticYearCalculateScheduler extends BaseMessageListener {
+
+	protected Log _log = LogFactoryUtil.getLog(DossierStatisticYearCalculateScheduler.class);
+
+	private static boolean IS_ENABLED =  Validator.isNotNull(PropsUtil.get("org.opencps.statistic.year.calculator.enabled")) ?
+			Boolean.valueOf(PropsUtil.get("org.opencps.statistic.year.calculator.enabled")) :false;
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+
+		if(IS_ENABLED){
+
+			List<Group> groups = GroupLocalServiceUtil.getGroups(PortalUtil.getDefaultCompanyId(),0,true);
+			/* 0 0 1 1 1/1 ? * */
+
+			long groupId = 0;
+			long year = 0;
+			StatisticEngineUpdateAction engineUpdateAction = new StatisticEngineUpdateAction();
+
+			Calendar now = Calendar.getInstance();
+			year = now.get(Calendar.YEAR)+2000;
+
+			for(Group group:groups) {
+
+				groupId = group.getGroupId();
+
+				_log.debug("+++groupId:" + groupId);
+				_log.debug("+++groupName:" + group.getDescriptiveName());
+				_log.debug("+++year:" + year);
+				_log.debug("+++time:" + System.currentTimeMillis());
+
+				engineUpdateAction.calculateStatistic(groupId, year);
+
+			}
+
+		}
+
+	}
+
+
+
+	/**
+	 * activate: Called whenever the properties for the component change (ala Config
+	 * Admin) or OSGi is activating the component.
+	 * 
+	 * @param properties The properties map from Config Admin.
+	 * @throws SchedulerException in case of error.
+	 */
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) throws SchedulerException {
+
+		String cronExpression = GetterUtil.getString(properties.get("cron.expression"), _DEFAULT_CRON_EXPRESSION);
+
+		// create a new trigger definition for the job.
+		String listenerClass = getClass().getName();
+		Trigger jobTrigger = _triggerFactory.createTrigger(listenerClass, listenerClass, new Date(), null,
+				cronExpression);
+
+		_schedulerEntryImpl = new SchedulerEntryImpl(getClass().getName(), jobTrigger);
+		_schedulerEntryImpl = new StorageTypeAwareSchedulerEntryImpl(_schedulerEntryImpl, StorageType.MEMORY_CLUSTERED);
+
+
+		if (_initialized) {
+			deactivate();
+		}
+
+		_schedulerEngineHelper.register(this, _schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+		_initialized = true;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_initialized) {
+			try {
+				_schedulerEngineHelper.unschedule(_schedulerEntryImpl, getStorageType());
+			} catch (SchedulerException se) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("Unable to unschedule trigger", se);
+				}
+			}
+
+			_schedulerEngineHelper.unregister(this);
+		}
+		_initialized = false;
+	}
+
+	/**
+	 * getStorageType: Utility method to get the storage type from the scheduler entry wrapper.
+	 * @return StorageType The storage type to use.
+	*/
+	protected StorageType getStorageType() {
+		if (_schedulerEntryImpl instanceof StorageTypeAware) {
+			return ((StorageTypeAware) _schedulerEntryImpl).getStorageType();
+		}
+
+		return StorageType.PERSISTED;
+	}
+
+	/**
+	 * setModuleServiceLifecycle: So this requires some explanation...
+	 * 
+	 * OSGi will start a component once all of it's dependencies are satisfied.
+	 * However, there are times where you want to hold off until the portal is
+	 * completely ready to go.
+	 * 
+	 * This reference declaration is waiting for the ModuleServiceLifecycle's
+	 * PORTAL_INITIALIZED component which will not be available until, surprise
+	 * surprise, the portal has finished initializing.
+	 * 
+	 * With this reference, this component activation waits until portal
+	 * initialization has completed.
+	 * 
+	 * @param moduleServiceLifecycle
+	 */
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setTriggerFactory(TriggerFactory triggerFactory) {
+		_triggerFactory = triggerFactory;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) {
+		_schedulerEngineHelper = schedulerEngineHelper;
+	}
+
+	private static final String _DEFAULT_CRON_EXPRESSION = "0 0 0 * * ?";
+	private SchedulerEngineHelper _schedulerEngineHelper;
+	private TriggerFactory _triggerFactory;
+	private volatile boolean _initialized;
+	private SchedulerEntryImpl _schedulerEntryImpl = null;
+}
