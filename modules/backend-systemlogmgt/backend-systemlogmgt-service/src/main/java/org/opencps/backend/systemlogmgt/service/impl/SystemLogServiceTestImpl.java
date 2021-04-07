bf03f0b0a9c11b24c8eb6949a921809228e4c004
doanvietcntt99 <doanvietcntt99@gmail.com>
2021-04-07 17:27:23 +0700

    update

--- modules/backend-systemlogmgt/backend-systemlogmgt-service/src/main/java/org/opencps/backend/systemlogmgt/service/impl/SystemLogServiceTestImpl.java
+++ modules/backend-systemlogmgt/backend-systemlogmgt-service/src/main/java/org/opencps/backend/systemlogmgt/service/impl/SystemLogServiceTestImpl.java
@@ -1,16 +1,14 @@
 package org.opencps.backend.systemlogmgt.service.impl;
 
 import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.StringUtil;
-import org.opencps.backend.systemlogmgt.constant.Constants;
-import org.opencps.backend.systemlogmgt.model.SystemLog;
-import org.opencps.backend.systemlogmgt.service.SystemLogLocalService;
 import org.opencps.backend.systemlogmgt.service.SystemLogLocalServiceUtil;
 import org.opencps.backend.systemlogmgt.service.SystemLogServiceTest;
-import org.opencps.backend.systemlogmgt.service.persistence.SystemLogPersistence;
-import org.opencps.backend.systemlogmgt.service.persistence.impl.SystemLogPersistenceImpl;
 
 import aQute.bnd.annotation.ProviderType;
 
@@ -33,30 +31,81 @@ public class SystemLogServiceTestImpl implements SystemLogServiceTest{
 	@Override
 	public void debug(Long groupId, String moduleName, String message, String... param) {
 		// TODO Auto-generated method stub
-		String type = "DEBUG";
+		String type = Thread.currentThread().getStackTrace()[1].getMethodName();
 		Integer preLine = Thread.currentThread().getStackTrace()[3].getLineNumber();
 		String preMethod = Thread.currentThread().getStackTrace()[3].getMethodName();
 		Integer line = Thread.currentThread().getStackTrace()[2].getLineNumber();
 		String method = Thread.currentThread().getStackTrace()[2].getMethodName();
 		String threadId = threadIdAuth.get();
-		String paramStr = null;
+		String paramInput = null;
 		if(Validator.isNotNull(param)) {
-			paramStr = StringUtil.merge(param);
+			JSONArray dataParam = null;
+			try {
+				dataParam = JSONFactoryUtil.createJSONArray(StringUtil.merge(param));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			for(int i=0; i<dataParam.length(); i++) {
+				JSONObject jsonObject = null;
+				try {
+					jsonObject = JSONFactoryUtil.createJSONObject(dataParam.get(i).toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				if(i==0) {
+					paramInput += jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				} else {
+					paramInput += "&" + jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				}
+			}
 		}
 		try {
-			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, message, type, threadId, paramStr);
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, message, type, threadId, paramInput);
 		} catch (Exception e) {
-			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, e.toString(), "ERROR", threadId, paramStr);
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, e.toString(), "ERROR", threadId, paramInput);
 		}
 	}
+	
 
 
 	@Override
 	public void error(Long groupId, String moduleName, String message, String... param) {
 		// TODO Auto-generated method stub
-		String type = "ERROR";
-		
+		String type = Thread.currentThread().getStackTrace()[1].getMethodName();
+		Integer preLine = Thread.currentThread().getStackTrace()[3].getLineNumber();
+		String preMethod = Thread.currentThread().getStackTrace()[3].getMethodName();
+		Integer line = Thread.currentThread().getStackTrace()[2].getLineNumber();
+		String method = Thread.currentThread().getStackTrace()[2].getMethodName();
+		String threadId = threadIdAuth.get();
+		String paramInput = null;
+		if(Validator.isNotNull(param)) {
+			JSONArray dataParam = null;
+			try {
+				dataParam = JSONFactoryUtil.createJSONArray(StringUtil.merge(param));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			for(int i=0; i<dataParam.length(); i++) {
+				JSONObject jsonObject = null;
+				try {
+					jsonObject = JSONFactoryUtil.createJSONObject(dataParam.get(i).toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				if(i==0) {
+					paramInput += jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				} else {
+					paramInput += "&" + jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				}
+			}
+		}
+		try {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, message, type, threadId, paramInput);
+		} catch (Exception e) {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, e.toString(), "ERROR", threadId, paramInput);
+		}
 	}
+	
 
 
 
@@ -65,8 +114,41 @@ public class SystemLogServiceTestImpl implements SystemLogServiceTest{
 	@Override
 	public void info(Long groupId, String moduleName, String message, String... param) {
 		// TODO Auto-generated method stub
-		String type = "INFO";
+		String type = Thread.currentThread().getStackTrace()[1].getMethodName();
+		Integer preLine = Thread.currentThread().getStackTrace()[3].getLineNumber();
+		String preMethod = Thread.currentThread().getStackTrace()[3].getMethodName();
+		Integer line = Thread.currentThread().getStackTrace()[2].getLineNumber();
+		String method = Thread.currentThread().getStackTrace()[2].getMethodName();
+		String threadId = threadIdAuth.get();
+		String paramInput = null;
+		if(Validator.isNotNull(param)) {
+			JSONArray dataParam = null;
+			try {
+				dataParam = JSONFactoryUtil.createJSONArray(StringUtil.merge(param));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			for(int i=0; i<dataParam.length(); i++) {
+				JSONObject jsonObject = null;
+				try {
+					jsonObject = JSONFactoryUtil.createJSONObject(dataParam.get(i).toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				if(i==0) {
+					paramInput += jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				} else {
+					paramInput += "&" + jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+				}
+			}
+		}
+		try {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, message, type, threadId, paramInput);
+		} catch (Exception e) {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, e.toString(), "ERROR", threadId, paramInput);
+		}
 	}
+	
 
 
 
@@ -75,7 +157,43 @@ public class SystemLogServiceTestImpl implements SystemLogServiceTest{
 	@Override
 	public void warn(Long groupId, String moduleName, String message, String... param) {
 		// TODO Auto-generated method stub
-		String type = "WARN";
+		String type = Thread.currentThread().getStackTrace()[1].getMethodName();
+		Integer preLine = Thread.currentThread().getStackTrace()[3].getLineNumber();
+		String preMethod = Thread.currentThread().getStackTrace()[3].getMethodName();
+		Integer line = Thread.currentThread().getStackTrace()[2].getLineNumber();
+		String method = Thread.currentThread().getStackTrace()[2].getMethodName();
+		String threadId = threadIdAuth.get();
+		String paramInput = null;
+		if(Validator.isNotNull(param)) {
+			JSONArray dataParam = null;
+			try {
+				dataParam = JSONFactoryUtil.createJSONArray(StringUtil.merge(param));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			for(int i=0; i<dataParam.length(); i++) {
+				JSONObject jsonObject = null;
+				try {
+					jsonObject = JSONFactoryUtil.createJSONObject(dataParam.get(i).toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				if(i==0) {
+					if(Validator.isNotNull(jsonObject.get("key"))) {
+						paramInput += jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+					}
+				} else {
+					if(Validator.isNotNull(jsonObject.get("key"))) {
+						paramInput += "&" + jsonObject.get("key").toString() + "=" + jsonObject.get("value").toString();
+					}
+				}
+			}
+		}
+		try {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, message, type, threadId, paramInput);
+		} catch (Exception e) {
+			SystemLogLocalServiceUtil.addSystemLog(groupId, moduleName, preLine, preMethod, line, method, e.toString(), "ERROR", threadId, paramInput);
+		}
 	}
 	
 }
