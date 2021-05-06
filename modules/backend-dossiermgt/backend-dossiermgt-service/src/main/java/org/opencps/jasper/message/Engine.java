c3e1e7211ad502bc0b0b12bc067c59c24197d5b9
duongnguyen <nh0xkid.chinhlata0@gmail.com>
2021-05-06 16:49:18 +0700

    Fix bug không xem được văn bản hành chính

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/jasper/message/Engine.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/jasper/message/Engine.java
@@ -76,9 +76,9 @@ public class Engine implements MessageListener {
 			_log.error(e);
 		}
 	}
-	
 
-	
+
+	private static final int MAX_TRY_COUNT = 3;
 	private void _doReceiveJasperRequest(Message message) {
 		// TODO Auto-generated method stub
 		_log.info("Dossier listener receive Jasper .............................");
@@ -222,6 +222,18 @@ public class Engine implements MessageListener {
 			else if (engineClass.isAssignableFrom(DossierDocument.class)) {
 //				DossierDocument dossierDocument = DossierDocumentLocalServiceUtil.fetchDossierDocument(classPK);
 				DossierDocument dossierDocument = DossierLocalServiceUtil.findDossierDocumentByDossierId(classPK);
+				int tryCount = 0;
+				while (dossierDocument == null) {
+					try {
+						Thread.sleep(2000);
+						dossierDocument = DossierLocalServiceUtil.findDossierDocumentByDossierId(classPK);
+						tryCount++;
+						if (tryCount == MAX_TRY_COUNT ) break;
+						if (dossierDocument != null ) break;
+					} catch (Exception e) {
+						e.getMessage();
+					}
+				}
 				_log.info("LOG DossierDocument: " + JSONFactoryUtil.looseSerialize(dossierDocument));
 				if (dossierDocument != null) {
 				ServiceContext serviceContext = new ServiceContext();
