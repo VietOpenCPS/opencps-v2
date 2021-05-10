67e0d552dafbb6abcf221f6b0afdc200a55c8d00
duongnguyen <nh0xkid.chinhlata0@gmail.com>
2021-05-10 17:55:52 +0700

    Trả thêm thông tin cho Token

--- wars/v1/src/main/java/org/graphql/api/controller/impl/RestfulController.java
+++ wars/v1/src/main/java/org/graphql/api/controller/impl/RestfulController.java
@@ -1497,6 +1497,7 @@ public class RestfulController {
 			if (Validator.isNull(userData)) {
 				throw new OpenCPSNotFoundException(User.class.getName());
 			}
+			_log.info("UserData: " + JSONFactoryUtil.looseSerialize(userData));
 //			List<Dossier> dossiers = DossierLocalServiceUtil.findByG_U_DO(groupId,userId);
 //			try {
 //				if (dossiers != null) {
@@ -1553,6 +1554,7 @@ public class RestfulController {
 //			}
 
 			String token = GraphQLUtils.buildTokenLogin(userData, groupId);
+			_log.info("Token: " + token);
 			response.setHeader("jwt-token", token);
 
 			return new ResponseEntity<UsersUserItem>(JSONFactoryUtil.looseDeserialize(userData, UsersUserItem.class),
--- wars/v1/src/main/java/org/graphql/api/controller/utils/GraphQLUtils.java
+++ wars/v1/src/main/java/org/graphql/api/controller/utils/GraphQLUtils.java
@@ -55,6 +55,7 @@ public class GraphQLUtils {
 						JobPos jobPos = JobPosLocalServiceUtil.fetchJobPos(empJobPos.getJobPostId());
 						roles.add(jobPos.getJobPosCode());
 					}
+					_log.info("User: " + JSONFactoryUtil.looseSerialize(user));
 					String token = JWT.create()
 							.withClaim("username", user.getString(UserTerm.SCREEN_NAME))
 							.withClaim("fullName", user.getString(UserTerm.EMPLOYEE_FULLNAME))
@@ -64,6 +65,8 @@ public class GraphQLUtils {
 					return token;
 				} else {
 					String token = JWT.create()
+							.withClaim("MaSoDoanhNghiep", user.getString(UserTerm.APPLICANT_ID_NO))
+							.withClaim("applicantIdType", user.getString(UserTerm.APPLICANT_TYPE))
 							.withClaim("username", user.getString(UserTerm.SCREEN_NAME))
 							.withClaim("fullName", user.getString(UserTerm.APPLICANT_NAME))
 							.withClaim("email", user.getString(UserTerm.APPLICANT_CONTACT_EMAIL))
