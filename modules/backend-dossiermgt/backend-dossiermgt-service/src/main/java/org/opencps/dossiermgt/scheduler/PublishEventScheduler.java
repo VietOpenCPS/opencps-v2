030e3576d1ef8a097ca8ff858ee22d201e984784
nhan0987 <skynetx002@gmail.com>
2021-05-17 08:54:30 +0700

    Restore code đồng bộ hồ sơ về multil thread

--- modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/scheduler/PublishEventScheduler.java
+++ modules/backend-dossiermgt/backend-dossiermgt-service/src/main/java/org/opencps/dossiermgt/scheduler/PublishEventScheduler.java
@@ -138,8 +138,9 @@ public class PublishEventScheduler extends BaseMessageListener {
 			_log.debug("OpenCPS PUBLISH DOSSIERS IS  : " + APIDateTimeUtils.convertDateToString(new Date()));
 
 			List<PublishQueue> lstPqs = PublishQueueLocalServiceUtil.getByStatusesAndNotServerNo(new int[] {
-							PublishQueueTerm.STATE_WAITING_SYNC},
-					ServerConfigTerm.DVCQG_INTEGRATION, 0, 180);
+							PublishQueueTerm.STATE_WAITING_SYNC,
+							PublishQueueTerm.STATE_ALREADY_SENT},
+					ServerConfigTerm.DVCQG_INTEGRATION, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 
 			_log.debug("lstPqs  : " + lstPqs.size());
 
@@ -217,7 +218,6 @@ public class PublishEventScheduler extends BaseMessageListener {
 				if(queueStatus == PublishQueueTerm.STATE_WAITING_SYNC) {
 					//start sync
 					oneQueue.setStatus(PublishQueueTerm.STATE_ALREADY_SENT);
-					oneQueue.setModifiedDate(new Date());
 					oneQueue = PublishQueueLocalServiceUtil.updatePublishQueue(oneQueue);
 
 					boolean result = processPublish(oneQueue);
@@ -315,7 +315,7 @@ public class PublishEventScheduler extends BaseMessageListener {
 
 					String payload = pq.getPublishData();
 					_log.debug("dossier Inform: " + dossier.getOriginDossierNo());
-					_log.debug("payload Inform: " + payload);
+					System.out.println("payload Inform: " + payload);
 					JSONObject payloadObj = JSONFactoryUtil.createJSONObject(payload);
 
 					// Sync file HSLT
